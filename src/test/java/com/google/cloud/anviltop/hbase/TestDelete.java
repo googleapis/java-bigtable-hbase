5fff9a6e6fabf3b15a0541b9b53c5acfa3eb4635
Carter Page <carterp@google.com>
2014-07-07 12:11:55 -0400

    Test deleting a specific version of a column.

--- src/test/java/com/google/cloud/anviltop/hbase/TestDelete.java
+++ src/test/java/com/google/cloud/anviltop/hbase/TestDelete.java
@@ -13,6 +13,7 @@
  */
 package com.google.cloud.anviltop.hbase;
 
+import org.apache.hadoop.hbase.Cell;
 import org.apache.hadoop.hbase.client.Delete;
 import org.apache.hadoop.hbase.client.Get;
 import org.apache.hadoop.hbase.client.HTableInterface;
@@ -22,6 +23,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.List;
 
 public class TestDelete extends AbstractTest {
   /**
@@ -49,7 +51,7 @@ public class TestDelete extends AbstractTest {
     // Delete row
     Delete delete = new Delete(rowKey);
     table.delete(delete);
-    Assert.assertFalse(table.exists(get));
+    Assert.assertFalse("Entire row should be deleted.", table.exists(get));
 
     table.close();
   }
@@ -64,7 +66,6 @@ public class TestDelete extends AbstractTest {
     byte[] rowKey = dataHelper.randomData("testrow-");
     byte[] qual = dataHelper.randomData("qual-");
     byte[] value = dataHelper.randomData("value-");
-
     Put put = new Put(rowKey);
     put.add(COLUMN_FAMILY, qual, 1L, value);
     put.add(COLUMN_FAMILY, qual, 2L, value);
@@ -81,10 +82,48 @@ public class TestDelete extends AbstractTest {
     delete.deleteColumn(COLUMN_FAMILY, qual);
     table.delete(delete);
 
-    // Confirm results
+    // Confirm results.
     result = table.get(get);
     Assert.assertEquals(1, result.size());
-    Assert.assertEquals(1L, result.getColumnLatestCell(COLUMN_FAMILY, qual).getTimestamp());
+    Assert.assertEquals("Version 2 should be deleted, but not version 1.",
+      1L, result.getColumnLatestCell(COLUMN_FAMILY, qual).getTimestamp());
+
+    table.close();
+  }
+
+  /**
+   * Requirement 4.3 - Delete a specific version of a specific column (family:qualifer + ts)
+   */
+  @Test
+  public void testDeleteSpecificColumnVersion() throws IOException {
+    // Initialize data
+    HTableInterface table = connection.getTable(TABLE_NAME);
+    byte[] rowKey = dataHelper.randomData("testrow-");
+    byte[] qual = dataHelper.randomData("qual-");
+    byte[] value = dataHelper.randomData("value-");
+    Put put = new Put(rowKey);
+    put.add(COLUMN_FAMILY, qual, 1L, value);
+    put.add(COLUMN_FAMILY, qual, 2L, value);
+    put.add(COLUMN_FAMILY, qual, 3L, value);
+    table.put(put);
+
+    // Check values
+    Get get = new Get(rowKey);
+    get.setMaxVersions(5);
+    Result result = table.get(get);
+    Assert.assertEquals(3, result.size());
+
+    // Delete latest column version
+    Delete delete = new Delete(rowKey);
+    delete.deleteColumn(COLUMN_FAMILY, qual, 2L);
+    table.delete(delete);
+
+    // Confirm results
+    result = table.get(get);
+    Assert.assertEquals("Only one version should be deleted", 2, result.size());
+    List<Cell> cells = result.getColumnCells(COLUMN_FAMILY, qual);
+    Assert.assertEquals("Version 3 should be the latest version", 3L, cells.get(0).getTimestamp());
+    Assert.assertEquals("Version 1 should be the oldest version", 1L, cells.get(1).getTimestamp());
 
     table.close();
   }
