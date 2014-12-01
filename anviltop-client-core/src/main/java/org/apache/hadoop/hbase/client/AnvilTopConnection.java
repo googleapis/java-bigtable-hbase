4f2b6dc743dd8749392aa328f91dcb6fbc1c06a7
mgarolera <mgaro@google.com>
2014-12-01 17:05:37 -0500

    Switch to use 0.99.2-SNAPSHOT

--- anviltop-client-core/src/main/java/org/apache/hadoop/hbase/client/AnvilTopConnection.java
+++ anviltop-client-core/src/main/java/org/apache/hadoop/hbase/client/AnvilTopConnection.java
@@ -289,7 +289,7 @@ public class AnvilTopConnection implements ClusterConnection, Closeable {
   }
 
   @Override
-  public HRegionLocation relocateRegion(TableName tableName, byte[] row, int replicaId)
+  public RegionLocations relocateRegion(TableName tableName, byte[] row, int replicaId)
       throws IOException {
     throw new UnsupportedOperationException();  // TODO
   }
@@ -475,6 +475,11 @@ public class AnvilTopConnection implements ClusterConnection, Closeable {
 
   @Override
   public AsyncProcess getAsyncProcess() {
+    throw new UnsupportedOperationException();  // TODO
+  }
+
+  @Override
+  public RpcRetryingCallerFactory getNewRpcRetryingCallerFactory(Configuration conf) {
     throw new UnsupportedOperationException();  // TODO
   }
 
