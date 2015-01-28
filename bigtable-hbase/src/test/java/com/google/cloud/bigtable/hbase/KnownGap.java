c60a3b60964e7840bc2b13ca2f762b3b7a55663f
mgarolera <mgaro@google.com>
2015-01-28 11:12:40 -0500

    Remove unnecessary import.

--- bigtable-hbase/src/test/java/com/google/cloud/bigtable/hbase/KnownGap.java
+++ bigtable-hbase/src/test/java/com/google/cloud/bigtable/hbase/KnownGap.java
@@ -1,6 +1,4 @@
 package com.google.cloud.bigtable.hbase;
 
-import org.junit.experimental.categories.Category;
-
 // Category for tests that are know to differ between hbase and cloud bigtable.
 public interface KnownGap {}
