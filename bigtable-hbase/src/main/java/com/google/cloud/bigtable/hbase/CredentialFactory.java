b40a03b9d7392509236b0aacaf18b85f7c1f03ba
Kevin Si <kevinsi4508@gmail.com>
2015-03-25 10:46:47 -0400

    remove unused imports.

--- bigtable-hbase/src/main/java/com/google/cloud/bigtable/hbase/CredentialFactory.java
+++ bigtable-hbase/src/main/java/com/google/cloud/bigtable/hbase/CredentialFactory.java
@@ -6,8 +6,6 @@ import java.security.GeneralSecurityException;
 import java.security.PrivateKey;
 import java.util.List;
 
-import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.googleapis.compute.ComputeCredential;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.HttpTransport;
