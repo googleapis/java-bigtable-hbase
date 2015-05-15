be44996668c53514215beaee80ec4d6687a05735
Solomon Duskis <sduskis@gmail.com>
2015-05-14 23:05:35 -0400

    Fixing TestImport clears the table first before performing the test.

--- bigtable-hbase-integration-tests/src/test/java/com/google/cloud/bigtable/hbase/TestImport.java
+++ bigtable-hbase-integration-tests/src/test/java/com/google/cloud/bigtable/hbase/TestImport.java
@@ -4,9 +4,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -43,22 +43,28 @@ public class TestImport extends AbstractTest {
   @Test
   @Category(KnownGap.class)
   public void testMapReduce() throws IOException, ClassNotFoundException, InterruptedException {
-    Table oldTable = getConnection().getTable(TABLE_NAME);
+    Admin admin = getConnection().getAdmin();
 
+    admin.disableTable(TABLE_NAME);
+    admin.deleteTable(TABLE_NAME);
+    IntegrationTests.createTable(TABLE_NAME);
     // Put a value.
     byte[] rowKey = dataHelper.randomData("testrow-");
     byte[] qual = dataHelper.randomData("testQualifier-");
     byte[] value = dataHelper.randomData("testValue-");
-    Put put = new Put(rowKey);
-    put.addColumn(COLUMN_FAMILY, qual, value);
-    oldTable.put(put);
 
-    // Assert the value is there.
-    Get get = new Get(rowKey);
-    Result result = oldTable.get(get);
-    List<Cell> cells = result.listCells();
-    Assert.assertEquals(1, cells.size());
-    Assert.assertArrayEquals(CellUtil.cloneValue(cells.get(0)), value);
+    try (Table oldTable = getConnection().getTable(TABLE_NAME)){
+      Put put = new Put(rowKey);
+      put.addColumn(COLUMN_FAMILY, qual, value);
+      oldTable.put(put);
+
+      // Assert the value is there.
+      Get get = new Get(rowKey);
+      Result result = oldTable.get(get);
+      List<Cell> cells = result.listCells();
+      Assert.assertEquals(1, cells.size());
+      Assert.assertArrayEquals(CellUtil.cloneValue(cells.get(0)), value);
+    }
 
     // Run the export.
     Configuration conf = getConnection().getConfiguration();
@@ -78,29 +84,31 @@ public class TestImport extends AbstractTest {
 
     // Create new table.
     TableName newTableName = IntegrationTests.newTestTableName();
-    Table newTable = getConnection().getTable(newTableName);
+    try (Table newTable = getConnection().getTable(newTableName)){
+      // Change for method in IntegrationTests
+      HColumnDescriptor hcd = new HColumnDescriptor(IntegrationTests.COLUMN_FAMILY);
+      HTableDescriptor htd = new HTableDescriptor(newTableName);
+      htd.addFamily(hcd);
+      admin.createTable(htd);
 
-    // Change for method in IntegrationTests
-    Admin admin = getConnection().getAdmin();
-    HColumnDescriptor hcd = new HColumnDescriptor(IntegrationTests.COLUMN_FAMILY);
-    HTableDescriptor htd = new HTableDescriptor(newTableName);
-    htd.addFamily(hcd);
-    admin.createTable(htd);
+      // Run the import.
+      args = new String[]{
+          newTableName.getNameAsString(),
+          tempDir
+      };
+      job = Import.createSubmittableJob(conf, args);
+      job.getConfiguration().set("fs.defaultFS", "file:///");
+      Assert.assertTrue(job.waitForCompletion(true));
 
-    // Run the import.
-    args = new String[]{
-        newTableName.getNameAsString(),
-        tempDir
-    };
-    job = Import.createSubmittableJob(conf, args);
-    job.getConfiguration().set("fs.defaultFS", "file:///");
-    Assert.assertTrue(job.waitForCompletion(true));
-
-    // Assert the value is there.
-    get = new Get(rowKey);
-    result = newTable.get(get);
-    cells = result.listCells();
-    Assert.assertEquals(1, cells.size());
-    Assert.assertArrayEquals(CellUtil.cloneValue(cells.get(0)), value);
+      // Assert the value is there.
+      Get get = new Get(rowKey);
+      Result result = newTable.get(get);
+      List<Cell> cells = result.listCells();
+      Assert.assertEquals(1, cells.size());
+      Assert.assertArrayEquals(CellUtil.cloneValue(cells.get(0)), value);
+    } finally {
+      admin.disableTable(newTableName);
+      admin.deleteTable(newTableName);
+    }
   }
 }
