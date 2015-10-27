4dc3a8b6c0cddf7552f44af21f222ebce55f094e
Solomon Duskis <sduskis@google.com>
2015-10-27 10:45:51 -0400

    Adding zeroCopy operation for RowAdapter.

--- bigtable-client-core/src/main/java/com/google/cloud/bigtable/util/ByteStringer.java
+++ bigtable-client-core/src/main/java/com/google/cloud/bigtable/util/ByteStringer.java
@@ -1,13 +1,13 @@
 package com.google.cloud.bigtable.util;
 
 import com.google.protobuf.ByteString;
-import com.google.protobuf.BigtableZeroCopyByteString;
+import com.google.protobuf.BigtableZeroCopyByteStringUtil;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 /**
- * Wrapper around {@link BigtableZeroCopyByteString} for cases where it's not available.
+ * Wrapper around {@link BigtableZeroCopyByteStringUtil} for cases where it's not available.
  */
 public class ByteStringer {
   private static final Log LOG = LogFactory.getLog(ByteStringer.class);
@@ -22,7 +22,7 @@ public class ByteStringer {
   // because it makes a copy of the passed in array.
   static {
     try {
-      BigtableZeroCopyByteString.wrap(new byte [0]);
+      BigtableZeroCopyByteStringUtil.wrap(new byte [0]);
     } catch (IllegalAccessError iae) {
       USE_ZEROCOPYBYTESTRING = false;
       LOG.debug("Failed to classload BigtableZeroCopyByteString: " + iae.toString());
@@ -37,14 +37,20 @@ public class ByteStringer {
    * Wraps a byte array in a {@link ByteString} without copying it.
    */
   public static ByteString wrap(final byte[] array) {
-    return USE_ZEROCOPYBYTESTRING? BigtableZeroCopyByteString.wrap(array): ByteString.copyFrom(array);
+    return USE_ZEROCOPYBYTESTRING? BigtableZeroCopyByteStringUtil.wrap(array): ByteString.copyFrom(array);
   }
   
   /**
    * Wraps a subset of a byte array in a {@link ByteString} without copying it.
    */
   public static ByteString wrap(final byte[] array, int offset, int length) {
-    return USE_ZEROCOPYBYTESTRING? BigtableZeroCopyByteString.wrap(array, offset, length):
+    return USE_ZEROCOPYBYTESTRING? BigtableZeroCopyByteStringUtil.wrap(array, offset, length):
       ByteString.copyFrom(array, offset, length);
+  }
+
+
+  public static byte[] extract(ByteString buf) {
+    return USE_ZEROCOPYBYTESTRING ? BigtableZeroCopyByteStringUtil.zeroCopyGetBytes(buf) : buf
+        .toByteArray();
   }
 }
