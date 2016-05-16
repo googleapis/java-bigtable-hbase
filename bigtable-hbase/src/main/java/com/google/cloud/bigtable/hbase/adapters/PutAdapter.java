dc86d06ed74fa1ae3dc56398ff114a365d8864a6
Solomon Duskis <sduskis@google.com>
2016-05-16 09:56:13 -0400

    Adding a Mutation -> Put adapter. This will help remove the HBase oriented mutation coder, which cases problems. TODO: - do the same thing for the Delete adapter - do the same thing for the Result adapter - update the Dataflow coders - upgrade to grpc 0.14.0, along with appropriate netty, proto and tcnative changes.

--- bigtable-hbase/src/main/java/com/google/cloud/bigtable/hbase/adapters/PutAdapter.java
+++ bigtable-hbase/src/main/java/com/google/cloud/bigtable/hbase/adapters/PutAdapter.java
@@ -17,14 +17,19 @@ package com.google.cloud.bigtable.hbase.adapters;
 
 import com.google.bigtable.v1.MutateRowRequest;
 import com.google.bigtable.v1.Mutation;
+import com.google.bigtable.v1.Mutation.MutationCase;
+import com.google.bigtable.v1.Mutation.SetCell;
 import com.google.bigtable.v1.Mutation.SetCell.Builder;
 import com.google.cloud.bigtable.hbase.BigtableConstants;
+import com.google.cloud.bigtable.hbase.adapters.read.RowCell;
+import com.google.protobuf.BigtableZeroCopyByteStringUtil;
 import com.google.protobuf.ByteString;
 
 import org.apache.hadoop.hbase.Cell;
 import org.apache.hadoop.hbase.HConstants;
 import org.apache.hadoop.hbase.client.Put;
 
+import java.io.IOException;
 import java.util.List;
 import java.util.Map.Entry;
 
@@ -47,12 +52,13 @@ public class PutAdapter implements OperationAdapter<Put, MutateRowRequest.Builde
 
   @Override
   public MutateRowRequest.Builder adapt(Put operation) {
-    MutateRowRequest.Builder result = MutateRowRequest.newBuilder();
-    result.setRowKey(ByteString.copyFrom(operation.getRow()));
 
     if (operation.isEmpty()) {
       throw new IllegalArgumentException("No columns to insert");
     }
+
+    MutateRowRequest.Builder result = MutateRowRequest.newBuilder();
+    result.setRowKey(ByteString.copyFrom(operation.getRow()));
 
     // Bigtable uses a 1ms granularity. Use this timestamp if the Put does not have one specified to
     // make mutations idempotent.
@@ -101,5 +107,35 @@ public class PutAdapter implements OperationAdapter<Put, MutateRowRequest.Builde
     }
 
     return result;
+  }
+
+  public Put adapt(MutateRowRequest request) throws IOException {
+    if (request.getMutationsCount() == 0) {
+      throw new IllegalArgumentException("No columns to insert");
+    }
+
+    byte[] rowkeyArray = request.getRowKey().toByteArray();
+    Put put = new Put(rowkeyArray);
+    for (Mutation mutation : request.getMutationsList()) {
+      if (mutation.getMutationCase() != MutationCase.SET_CELL) {
+        throw new IllegalArgumentException(
+            "Cannot process mutation of type: " + mutation.getMutationCase());
+      }
+      SetCell setCell = mutation.getSetCell();
+      long timestampHbase;
+      if (setCell.getTimestampMicros() == -1) {
+        timestampHbase = HConstants.LATEST_TIMESTAMP;
+      } else {
+        timestampHbase = BigtableConstants.HBASE_TIMEUNIT.convert(setCell.getTimestampMicros(),
+          BigtableConstants.BIGTABLE_TIMEUNIT);
+      }
+      put.add(new RowCell(rowkeyArray, getBytes(setCell.getFamilyNameBytes()),
+          getBytes(setCell.getColumnQualifier()), timestampHbase, getBytes(setCell.getValue())));
+    }
+    return put;
+  }
+
+  private static byte[] getBytes(ByteString bs) {
+    return BigtableZeroCopyByteStringUtil.zeroCopyGetBytes(bs);
   }
 }
