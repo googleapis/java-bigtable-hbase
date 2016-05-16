3314039e35573985c267988a53776d9adb77a617
Solomon Duskis <sduskis@google.com>
2016-05-16 14:32:16 -0400

    RowAdapter can now convert Results to Rows.

--- bigtable-hbase/src/main/java/com/google/cloud/bigtable/hbase/adapters/read/RowAdapter.java
+++ bigtable-hbase/src/main/java/com/google/cloud/bigtable/hbase/adapters/read/RowAdapter.java
@@ -4,9 +4,9 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  *     http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -15,6 +15,16 @@
  */
 package com.google.cloud.bigtable.hbase.adapters.read;
 
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.NavigableMap;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+import org.apache.hadoop.hbase.KeyValue;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.util.Bytes;
+
 import com.google.bigtable.v1.Cell;
 import com.google.bigtable.v1.Column;
 import com.google.bigtable.v1.Family;
@@ -22,16 +32,10 @@ import com.google.bigtable.v1.Row;
 import com.google.cloud.bigtable.hbase.BigtableConstants;
 import com.google.cloud.bigtable.hbase.adapters.ResponseAdapter;
 import com.google.cloud.bigtable.util.ByteStringer;
-
-import org.apache.hadoop.hbase.KeyValue;
-import org.apache.hadoop.hbase.client.Result;
-import org.apache.hadoop.hbase.util.Bytes;
-
-import java.util.SortedSet;
-import java.util.TreeSet;
+import com.google.protobuf.BigtableZeroCopyByteStringUtil;
 
 /**
- * Adapt a bigtable.v1.Row to an hbase client Result.
+ * Adapt between a {@link Row} and an hbase client {@link Result}.
  */
 public class RowAdapter implements ResponseAdapter<Row, Result> {
   // This only works because BIGTABLE_TIMEUNIT is smaller than HBASE_TIMEUNIT, otherwise we will get
@@ -39,6 +43,9 @@ public class RowAdapter implements ResponseAdapter<Row, Result> {
   static final long TIME_CONVERSION_UNIT = BigtableConstants.BIGTABLE_TIMEUNIT.convert(1,
     BigtableConstants.HBASE_TIMEUNIT);
 
+  /**
+   * Convert a {@link Row} to a {@link Result}.
+   */
   @Override
   public Result adaptResponse(Row response) {
     if (response == null) {
@@ -78,5 +85,46 @@ public class RowAdapter implements ResponseAdapter<Row, Result> {
     }
 
     return Result.create(hbaseCells.toArray(new org.apache.hadoop.hbase.Cell[hbaseCells.size()]));
+  }
+
+  /**
+   * Convert a {@link Result} to a {@link Row}.
+   */
+  public Row adaptToRow(Result result) {
+    Row.Builder rowBuilder = Row.newBuilder();
+
+    // Result.getRow() is derived from its cells.  If the cells are empty, the row will be null.
+    if (result.getRow() != null) {
+      rowBuilder.setKey(BigtableZeroCopyByteStringUtil.wrap(result.getRow()));
+    }
+
+    Map<byte[], NavigableMap<byte[], NavigableMap<Long, byte[]>>> familyMap = result.getMap();
+
+    // If there are no cells, the family map is null.
+    if (familyMap != null) {
+
+      // process all families
+      for (Entry<byte[], NavigableMap<byte[], NavigableMap<Long, byte[]>>> familyEntry :
+           familyMap.entrySet()) {
+        Family.Builder familyBuilder =
+            rowBuilder.addFamiliesBuilder().setName(Bytes.toString(familyEntry.getKey()));
+
+        // process the columns in the family
+        for (Entry<byte[], NavigableMap<Long, byte[]>> columnEntry :
+            familyEntry.getValue().entrySet()) {
+          Column.Builder columnBuilder = familyBuilder.addColumnsBuilder()
+              .setQualifier(BigtableZeroCopyByteStringUtil.wrap(columnEntry.getKey()));
+
+          // process the cells in the column
+          for (Entry<Long, byte[]> cellData : columnEntry.getValue().entrySet()) {
+            columnBuilder.addCellsBuilder()
+                .setTimestampMicros(cellData.getKey().longValue() * TIME_CONVERSION_UNIT)
+                .setValue(BigtableZeroCopyByteStringUtil.wrap(cellData.getValue()));
+          }
+        }
+      }
+    }
+
+    return rowBuilder.build();
   }
 }
