75c8be73cf08dadf2f9f351aa6b16b1bb55c0b13
Solomon Duskis <sduskis@google.com>
2016-09-14 23:32:21 -0400

    Adding dynamic rebalancing to the CloudBigtableIO.Reader. (#1007)
    * Adding dynamic rebalancing to the CloudBigtableIO.Reader.
    This replaces #997 which has some merge conflicts.  This also has some unit testing.
    
    * Upgrading to Dataflow 1.7.0

--- bigtable-dataflow-parent/bigtable-hbase-dataflow/src/test/java/com/google/cloud/bigtable/dataflow/CloudBigtableIOReaderTest.java
+++ bigtable-dataflow-parent/bigtable-hbase-dataflow/src/test/java/com/google/cloud/bigtable/dataflow/CloudBigtableIOReaderTest.java
@@ -29,8 +29,13 @@ import org.mockito.MockitoAnnotations;
 
 import com.google.bigtable.repackaged.com.google.cloud.grpc.BigtableSession;
 import com.google.bigtable.repackaged.com.google.cloud.grpc.scanner.ResultScanner;
+import com.google.bigtable.repackaged.com.google.com.google.bigtable.v2.ReadRowsRequest;
 import com.google.bigtable.repackaged.com.google.com.google.bigtable.v2.Row;
 import com.google.bigtable.repackaged.com.google.protobuf.ByteString;
+import com.google.cloud.bigtable.dataflow.CloudBigtableScanConfiguration.Builder;
+import com.google.cloud.dataflow.sdk.io.range.ByteKey;
+import com.google.cloud.dataflow.sdk.io.range.ByteKeyRange;
+import com.google.cloud.dataflow.sdk.io.range.ByteKeyRangeTracker;
 
 /**
  * Tests for {@link CloudBigtableIO.Reader}.
@@ -51,24 +56,20 @@ public class CloudBigtableIOReaderTest {
   @Before
   public void setup(){
     MockitoAnnotations.initMocks(this);
-    CloudBigtableScanConfiguration config = new CloudBigtableScanConfiguration.Builder()
-        .withProjectId("test").withInstanceId("test").withTableId("test").build();
-    when(mockSource.getConfiguration()).thenReturn(config);
+  }
+
+  private Builder createDefaultConfig() {
+    return new CloudBigtableScanConfiguration.Builder().withProjectId("test").withInstanceId("test")
+        .withTableId("test").withRequest(ReadRowsRequest.getDefaultInstance())
+        .withKeys(new byte[0], new byte[0]);
   }
 
   @Test
   public void testBasic() throws IOException {
-    CloudBigtableIO.Reader<Result> underTest =
-        new CloudBigtableIO.Reader<Result>(mockSource, CloudBigtableIO.RESULT_ADVANCER) {
-          @Override
-          void initializeScanner() throws IOException {
-            setSession(mockSession);
-            setScanner(mockScanner);
-          }
-        };
 
-    ByteString key = ByteString.copyFrom(Bytes.toBytes("a"));
-    when(mockScanner.next()).thenReturn(Row.newBuilder().setKey(key).build());
+    CloudBigtableIO.Reader<Result> underTest = initializeReader(createDefaultConfig().build());
+
+    setRowKey("a");
     Assert.assertTrue(underTest.start());
     Assert.assertEquals(1, underTest.getRowsReadCount());
 
@@ -77,5 +78,54 @@ public class CloudBigtableIOReaderTest {
     Assert.assertEquals(1, underTest.getRowsReadCount());
 
     underTest.close();
+  }
+
+  private void setRowKey(String rowKey) throws IOException {
+    ByteString rowKeyByteString = ByteString.copyFrom(Bytes.toBytes(rowKey));
+    Row row = Row.newBuilder().setKey(rowKeyByteString).build();
+    when(mockScanner.next()).thenReturn(row);
+  }
+
+  private CloudBigtableIO.Reader<Result> initializeReader(CloudBigtableScanConfiguration config) {
+    when(mockSource.getConfiguration()).thenReturn(config);
+    return new CloudBigtableIO.Reader<Result>(mockSource, CloudBigtableIO.RESULT_ADVANCER) {
+      @Override
+      void initializeScanner() throws IOException {
+        setSession(mockSession);
+        setScanner(mockScanner);
+      }
+    };
+  }
+
+  @Test
+  public void testPercent() throws IOException{
+    byte[] start = "aa".getBytes();
+    byte[] end = "zz".getBytes();
+    CloudBigtableScanConfiguration config = createDefaultConfig().withKeys(start, end).build();
+    CloudBigtableIO.Reader<Result> underTest = initializeReader(config);
+    ByteKeyRangeTracker tracker =
+        ByteKeyRangeTracker.of(ByteKeyRange.of(ByteKey.copyFrom(start), ByteKey.copyFrom(end)));
+
+    testTrackerAtKey(underTest, tracker, "bb", 1);
+    testTrackerAtKey(underTest, tracker, "qq", 2);
+
+    double splitAtFraction =
+        (1 - tracker.getFractionConsumed()) * .5 + tracker.getFractionConsumed();
+    ByteKey newSplitEnd = config.toByteKeyRange().interpolateKey(splitAtFraction);
+
+    underTest.splitAtFraction(splitAtFraction);
+    tracker.trySplitAtPosition(newSplitEnd);
+
+    Assert.assertEquals(tracker.getFractionConsumed(), underTest.getFractionConsumed(), 0.0001d);
+  }
+
+  private void testTrackerAtKey(CloudBigtableIO.Reader<Result> underTest, ByteKeyRangeTracker tracker,
+      final String key, final int count) throws IOException {
+    setRowKey(key);
+    tracker.tryReturnRecordAt(true, ByteKey.copyFrom(key.getBytes()));
+    Assert.assertTrue(underTest.start());
+    Assert.assertEquals(count, underTest.getRowsReadCount());
+    Assert.assertEquals(tracker.getFractionConsumed(),
+      underTest.getFractionConsumed().doubleValue(), .001d);
   }
 }
