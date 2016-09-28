72d9be105ac6b8959c792ebee156d7980ab987e7
Solomon Duskis <sduskis@google.com>
2016-09-28 17:55:26 -0400

    Readding zero copy which significantly improves reads. (#1028)
    * Readding zero copy which significantly improves reads.
    
    * Updating the RowMerger microbenchmark.
    
    * Fixing formatting in RowMergerPerf.
    
    * Fixing a typo in RowMergerPerf.
    
    * Adding some Preconditions in RowMergerPerf.java.

--- bigtable-hbase-parent/bigtable-hbase/src/test/java/com/google/cloud/bigtable/hbase/RowMergerPerf.java
+++ bigtable-hbase-parent/bigtable-hbase/src/test/java/com/google/cloud/bigtable/hbase/RowMergerPerf.java
@@ -7,10 +7,13 @@ import org.apache.commons.lang.RandomStringUtils;
 import org.apache.hadoop.hbase.util.Bytes;
 
 import com.google.bigtable.v2.ReadRowsResponse;
+import com.google.bigtable.v2.ReadRowsResponse.Builder;
 import com.google.bigtable.v2.ReadRowsResponse.CellChunk;
+import com.google.bigtable.v2.Row;
 import com.google.cloud.bigtable.grpc.scanner.RowMerger;
 import com.google.cloud.bigtable.hbase.adapters.Adapters;
 import com.google.cloud.bigtable.hbase.adapters.read.RowAdapter;
+import com.google.common.base.Preconditions;
 import com.google.protobuf.ByteString;
 import com.google.protobuf.BytesValue;
 import com.google.protobuf.StringValue;
@@ -21,26 +24,41 @@ import com.google.protobuf.StringValue;
 public class RowMergerPerf {
 
   public static void main(String[] args) {
-    for (int i = 0; i < 10; i++) {
-      rowMergerPerf(createResponses());
+    for (int i = 0; i < 5; i++) {
+      rowMergerPerf(createResponses(1));
+    }
+    for (int i = 5; i <= 100; i += 10) {
+      System.out.println("===================");
+      System.out.println("using " + i + " Cells");
+      rowMergerPerf(createResponses(i));
     }
   }
 
-  private static List<ReadRowsResponse> createResponses() {
+  private static List<ReadRowsResponse> createResponses(int cellCount) {
+    Builder readRowsResponse = ReadRowsResponse.newBuilder();
 
-    CellChunk contentChunk = CellChunk.newBuilder()
-        .setFamilyName(StringValue.newBuilder().setValue("Family1"))
-        .setQualifier(BytesValue.newBuilder().setValue(ByteString.copyFromUtf8("Qaulifier")))
-        .setRowKey(ByteString.copyFrom(Bytes.toBytes("rowkey-0")))
-        .setValue(ByteString.copyFrom(RandomStringUtils.randomAlphanumeric(10000).getBytes()))
-        .setTimestampMicros(0L)
-        .setCommitRow(true)
-        .build();
+    Preconditions.checkArgument(cellCount > 0, "cellCount has to be > 0.");
 
-    return Arrays.asList(ReadRowsResponse.newBuilder().addChunks(contentChunk).build());
+    // It's ok if 100_000 / cellCount rounds down.  This only has to be approximate.
+    int size = 100_000 / cellCount;
+    Preconditions.checkArgument(size > 0, "size has to be > 0.");
+    for (int i = 0; i < cellCount; i++) {
+      CellChunk contentChunk =
+          CellChunk.newBuilder()
+              .setFamilyName(StringValue.newBuilder().setValue("Family" + i))
+              .setQualifier(BytesValue.newBuilder().setValue(ByteString.copyFromUtf8("Qualifier")))
+              .setRowKey(ByteString.copyFrom(Bytes.toBytes("rowkey-0")))
+              .setValue(ByteString.copyFrom(RandomStringUtils.randomAlphanumeric(size).getBytes()))
+              .setTimestampMicros(0L)
+              .setCommitRow(i == cellCount - 1)
+              .build();
+
+      readRowsResponse.addChunks(contentChunk);
+    }
+    return Arrays.asList(readRowsResponse.build());
   }
 
-  static int count = 500_000;
+  static int count = 1_000_000;
 
   private static void rowMergerPerf(List<ReadRowsResponse> responses) {
     RowAdapter adapter = Adapters.ROW_ADAPTER;
@@ -57,12 +75,13 @@ public class RowMergerPerf {
     }
     {
       long start = System.nanoTime();
+      Row response = RowMerger.toRows(responses).get(0);
       for (int i = 0; i < count; i++) {
-        adapter.adaptResponse(RowMerger.toRows(responses).get(0));
+        adapter.adaptResponse(response);
       }
       long time = System.nanoTime() - start;
       System.out.println(
-          String.format("RowMerger + adaptResponse: %d rows merged in %d ms.  %d nanos per row.",
+          String.format("AdaptResponse: %d rows adapted in %d ms.  %d nanos per row.",
               count, time / 1000000, time / count));
     }
   }
