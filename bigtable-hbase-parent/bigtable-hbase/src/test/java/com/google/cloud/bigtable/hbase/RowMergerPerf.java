6a1063932f0e2190e7d2314c052d77a278aa4f1f
Solomon Duskis <sduskis@google.com>
2016-09-28 16:41:48 -0400

    Fixing formatting in RowMergerPerf.

--- bigtable-hbase-parent/bigtable-hbase/src/test/java/com/google/cloud/bigtable/hbase/RowMergerPerf.java
+++ bigtable-hbase-parent/bigtable-hbase/src/test/java/com/google/cloud/bigtable/hbase/RowMergerPerf.java
@@ -23,7 +23,7 @@ import com.google.protobuf.StringValue;
 public class RowMergerPerf {
 
   public static void main(String[] args) {
-    for(int i =0; i<5; i++) {
+    for (int i = 0; i < 5; i++) {
       rowMergerPerf(createResponses(1));
     }
     for (int i = 1; i <= 101; i += 10) {
