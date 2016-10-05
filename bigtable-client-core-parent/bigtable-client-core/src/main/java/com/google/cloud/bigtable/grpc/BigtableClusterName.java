30a0ca3257c829a1e76a9f13dbe330c1c435bd72
Gary Elliott <garyelliott@google.com>
2016-10-05 11:33:18 -0400

    Refactor BigtableClusterName and add a license

--- bigtable-client-core-parent/bigtable-client-core/src/main/java/com/google/cloud/bigtable/grpc/BigtableClusterName.java
+++ bigtable-client-core-parent/bigtable-client-core/src/main/java/com/google/cloud/bigtable/grpc/BigtableClusterName.java
@@ -1,16 +1,42 @@
+/*
+ * Copyright 2016 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.google.cloud.bigtable.grpc;
 
 import com.google.common.base.Preconditions;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * This class encapsulates a cluster name of the form
  * projects/(projectId)/instances/(instanceId)/clusters/(clusterId)
  */
 public class BigtableClusterName {
+  // Use a very loose pattern so we don't validate more strictly than the server.
+  private static final Pattern PATTERN =
+      Pattern.compile("projects/.*/instances/(.*)/clusters/.*");
+
   private final String clusterName;
+  private final String instanceId;
 
   public BigtableClusterName(String clusterName) {
     this.clusterName = clusterName;
+    Matcher matcher = PATTERN.matcher(clusterName);
+    Preconditions.checkArgument(matcher.matches(), "Malformed cluster name");
+    this.instanceId = matcher.group(1);
   }
 
   @Override
@@ -22,8 +48,6 @@ public class BigtableClusterName {
    * @return The id of the instance that contains this cluster.
    */
   public String getInstanceId() {
-    String[] parts = clusterName.split("/");
-    Preconditions.checkState(parts.length == 6, "Malformed cluster name: " + clusterName);
-    return parts[3];
+    return instanceId;
   }
 }
