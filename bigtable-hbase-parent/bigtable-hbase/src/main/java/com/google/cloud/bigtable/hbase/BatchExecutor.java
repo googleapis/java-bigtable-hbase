1967b194e5b7a061a13762242d9a070fadc3063e
Igor Bernstein <igorbernstein@google.com>
2017-02-06 15:54:33 -0500

    remove stray changes

--- bigtable-hbase-parent/bigtable-hbase/src/main/java/com/google/cloud/bigtable/hbase/BatchExecutor.java
+++ bigtable-hbase-parent/bigtable-hbase/src/main/java/com/google/cloud/bigtable/hbase/BatchExecutor.java
@@ -15,13 +15,11 @@
  */
 package com.google.cloud.bigtable.hbase;
 
-import com.google.cloud.bigtable.grpc.BigtableSessionSharedThreadPools;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
-import java.util.concurrent.ExecutorService;
 import javax.annotation.Nullable;
 
 import org.apache.hadoop.hbase.client.Append;
