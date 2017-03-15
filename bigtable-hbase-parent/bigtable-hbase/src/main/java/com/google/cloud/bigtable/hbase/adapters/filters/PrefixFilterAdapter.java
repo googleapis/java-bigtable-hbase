1810c2384dbd579e603a9b0f077a549b47c84661
Igor Bernstein <igorbernstein@google.com>
2017-03-15 17:02:10 -0400

    Optimize prefix filter (#1237)
    * add index scan hints to prefix filter
    * extend prefix integration test

--- bigtable-hbase-parent/bigtable-hbase/src/main/java/com/google/cloud/bigtable/hbase/adapters/filters/PrefixFilterAdapter.java
+++ bigtable-hbase-parent/bigtable-hbase/src/main/java/com/google/cloud/bigtable/hbase/adapters/filters/PrefixFilterAdapter.java
@@ -18,11 +18,14 @@ package com.google.cloud.bigtable.hbase.adapters.filters;
 import com.google.bigtable.v2.RowFilter;
 import com.google.cloud.bigtable.hbase.adapters.read.ReaderExpressionHelper;
 import com.google.cloud.bigtable.util.ByteStringer;
+import com.google.cloud.bigtable.util.RowKeyUtil;
+import com.google.cloud.bigtable.util.RowKeyWrapper;
+import com.google.common.collect.ImmutableRangeSet;
+import com.google.common.collect.Range;
+import com.google.common.collect.RangeSet;
 import com.google.protobuf.ByteString;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.util.Arrays;
-import org.apache.hadoop.hbase.HConstants;
 import org.apache.hadoop.hbase.filter.PrefixFilter;
 
 /**
@@ -32,7 +35,10 @@ import org.apache.hadoop.hbase.filter.PrefixFilter;
  * @version $Id: $Id
  */
 public class PrefixFilterAdapter extends TypedFilterAdapterBase<PrefixFilter> {
-  /** {@inheritDoc} */
+
+  /**
+   * {@inheritDoc}
+   */
   @Override
   public RowFilter adapt(FilterAdapterContext context, PrefixFilter filter)
       throws IOException {
@@ -46,10 +52,27 @@ public class PrefixFilterAdapter extends TypedFilterAdapterBase<PrefixFilter> {
         .build();
   }
 
-  /** {@inheritDoc} */
+  /**
+   * {@inheritDoc}
+   */
   @Override
   public FilterSupportStatus isFilterSupported(
       FilterAdapterContext context, PrefixFilter filter) {
     return FilterSupportStatus.SUPPORTED;
+  }
+
+  @Override
+  public RangeSet<RowKeyWrapper> getIndexScanHint(PrefixFilter filter) {
+    if (filter.getPrefix().length == 0) {
+      return ImmutableRangeSet.of(Range.<RowKeyWrapper>all());
+    } else {
+      ByteString start = ByteString.copyFrom(filter.getPrefix());
+      ByteString end = ByteString.copyFrom(
+          RowKeyUtil.calculateTheClosestNextRowKeyForPrefix(filter.getPrefix())
+      );
+      return ImmutableRangeSet.of(
+          Range.closedOpen(new RowKeyWrapper(start), new RowKeyWrapper(end))
+      );
+    }
   }
 }
