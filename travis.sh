618db82b5427091ac30ee182734197823d7bfb6c
Igor Bernstein <igorbernstein@google.com>
2017-03-22 13:34:45 -0400

    revert travis integration tests...needs a bit more ressearch

--- travis.sh
+++ travis.sh
@@ -6,14 +6,7 @@ set -x  # display all commands
 MVN="mvn --batch-mode"
 
 # build & run tests
-$MVN clean install \
+$MVN clean verify \
   | egrep -v "(^\[INFO\] Download|^\[INFO\].*skipping)"
 
-# run integration tests using a specific hbase version
-$MVN verify \
-  -pl bigtable-hbase-parent/bigtable-hbase-integration-tests \
-  -PhbaseLocalMiniClusterTest \
-  -Dhbase.version=${INT_HBASE:-1.3.0} \
-  -Dgoogle.bigtable.project.under.test=${INT_TARGET:-bigtable-hbase-1.3} \
-  -Dgoogle.bigtable.connection.impl=${INT_CONN:-com.google.cloud.bigtable.hbase1_3.BigtableConnection} \
-  | egrep -v "(^\[INFO\] Download|^\[INFO\].*skipping)"
+# TODO(igorbernstein2): enable integration tests
