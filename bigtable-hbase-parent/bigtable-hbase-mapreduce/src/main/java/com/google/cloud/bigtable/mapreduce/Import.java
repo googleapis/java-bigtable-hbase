3029eb06b29d6888bf3fc67ff7b6e29b57cfc8a6
Igor Bernstein <igorbernstein@google.com>
2017-06-08 10:21:07 -0400

    update hbase-mapreduce to use the 1.x artifact

--- bigtable-hbase-parent/bigtable-hbase-mapreduce/src/main/java/com/google/cloud/bigtable/mapreduce/Import.java
+++ bigtable-hbase-parent/bigtable-hbase-mapreduce/src/main/java/com/google/cloud/bigtable/mapreduce/Import.java
@@ -16,7 +16,7 @@
 package com.google.cloud.bigtable.mapreduce;
 
 import com.google.cloud.bigtable.hbase.BigtableOptionsFactory;
-import com.google.cloud.bigtable.hbase1_2.BigtableConnection;
+import com.google.cloud.bigtable.hbase1_x.BigtableConnection;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
