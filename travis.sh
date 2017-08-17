9ef433df8a1501f07665a0bdfc430d998372c598
Igor Bernstein <igorbernstein@google.com>
2017-08-17 16:35:04 -0400

    disable install step and go back to the travis.sh script

---
+++ travis.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e  # exit immediately on error
+set -x  # display all commands
+
+# Limit the amount of memory maven can use to avoid hitting the 3GB build limit in travis
+export MAVEN_OPTS="-Xmx1024m"
+
+# build & run tests
+/usr/bin/time mvn --batch-mode \
+  clean verify \
+  | egrep -v "(^\[INFO\] Download|^\[INFO\].*skipping)"
+
+# TODO(igorbernstein2): enable integration tests
