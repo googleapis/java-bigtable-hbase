7a2cec10bd5135e1c4baa7b75dec68b6c494dbe5
Solomon Duskis <sduskis@google.com>
2017-08-31 10:56:24 -0400

    Opencensus initial testing.

--- bigtable-client-core-parent/bigtable-client-core/src/test/java/com/google/cloud/bigtable/grpc/IntegrationBigtableSessionTest.java
+++ bigtable-client-core-parent/bigtable-client-core/src/test/java/com/google/cloud/bigtable/grpc/IntegrationBigtableSessionTest.java
@@ -22,6 +22,10 @@ import org.junit.runners.JUnit4;
 
 import com.google.bigtable.admin.v2.ListTablesRequest;
 import com.google.cloud.bigtable.config.BigtableOptions;
+import com.google.cloud.bigtable.config.Logger;
+
+import io.opencensus.contrib.zpages.ZPageHandlers;
+import io.opencensus.exporter.trace.stackdriver.StackdriverExporter;
 
 /**
  * Simple connectivity test to test that
@@ -31,13 +35,33 @@ import com.google.cloud.bigtable.config.BigtableOptions;
  */
 @RunWith(JUnit4.class)
 public class IntegrationBigtableSessionTest {
+  protected static final Logger LOG = new Logger(IntegrationBigtableSessionTest.class);
 
   @Test
   public void connectivityTest() {
+    try {
+      LOG.info("Initializing StackdriverExporter");
+      StackdriverExporter.createAndRegister("sduskis-hello-shakespear");
+    } catch (Throwable e) {
+      LOG.error("Could int register stackdriver", e);
+      Throwable t = e.getCause();
+      while(t != null) {
+        LOG.error("Could int register stackdriver ##", t);
+        t = t.getCause();
+      }
+    }
+    try {
+      LOG.info("Initializing ZPageHandlers");
+      ZPageHandlers.startHttpServerAndRegisterAll(100010);
+    } catch (Throwable e) {
+      LOG.error("Could int initialize ZPageHandlers", e);
+    }
     String property = System.getProperty("bigtableSession-int-test");
+
     if (!"true".equals(property)) {
       return;
     }
+    
     String projectId = System.getProperty("google.bigtable.project.id");
     String instanceId = System.getProperty("google.bigtable.instance.id");
 
