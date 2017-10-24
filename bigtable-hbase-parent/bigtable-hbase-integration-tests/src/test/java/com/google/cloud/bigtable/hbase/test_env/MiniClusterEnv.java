3a2fee965bbe6c025969bde263806467e41811e4
Igor Bernstein <igorbernstein@google.com>
2017-10-24 14:22:13 -0400

    Add some logging to debug integration test failures (#1521)

--- bigtable-hbase-parent/bigtable-hbase-integration-tests/src/test/java/com/google/cloud/bigtable/hbase/test_env/MiniClusterEnv.java
+++ bigtable-hbase-parent/bigtable-hbase-integration-tests/src/test/java/com/google/cloud/bigtable/hbase/test_env/MiniClusterEnv.java
@@ -16,6 +16,8 @@
 package com.google.cloud.bigtable.hbase.test_env;
 
 import java.io.IOException;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.HBaseConfiguration;
 import org.apache.hadoop.hbase.HBaseTestingUtility;
@@ -23,19 +25,27 @@ import org.apache.hadoop.hbase.client.Connection;
 import org.apache.hadoop.hbase.client.ConnectionFactory;
 
 class MiniClusterEnv extends SharedTestEnv {
+  private static final Log LOG = LogFactory.getLog(MiniClusterEnv.class);
 
   private HBaseTestingUtility helper;
 
   @Override
   protected void setup() throws Exception {
+    LOG.info("Starting hbase minicluster");
+
     helper = HBaseTestingUtility.createLocalHTU();
     helper.startMiniCluster();
+
+    LOG.info("Test dir: " + helper.getDataTestDir());
   }
 
   @Override
   protected void teardown() throws IOException {
     helper.shutdownMiniHBaseCluster();
-    helper.cleanupTestDir();
+    System.out.println("Cleaning up testDir: " + helper.getDataTestDir());
+    if (!helper.cleanupTestDir()) {
+      LOG.warn("Failed to clean up testDir");
+    }
     helper = null;
   }
 
