6dffa6d4f2df2be040cdc117b65990485567884b
Igor Bernstein <igorbernstein@google.com>
2017-12-20 12:25:08 -0500

    added a comment explaining the existence of a new test suite

--- bigtable-hbase-parent/bigtable-hbase-integration-tests/src/test/java/com/google/cloud/bigtable/hbase/IntegrationTestsReplication.java
+++ bigtable-hbase-parent/bigtable-hbase-integration-tests/src/test/java/com/google/cloud/bigtable/hbase/IntegrationTestsReplication.java
@@ -3,6 +3,9 @@ package com.google.cloud.bigtable.hbase;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
+/**
+ * A suite of tests that require the target project to be whitelisted for replication
+ */
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
     TestReplicationAppProfile.class
