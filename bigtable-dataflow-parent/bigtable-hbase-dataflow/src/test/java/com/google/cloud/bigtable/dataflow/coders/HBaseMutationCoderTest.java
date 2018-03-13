9e21829df4fd86b327b794e201656e95d84a23f8
Solomon Duskis <sduskis@google.com>
2018-03-13 10:56:07 -0400

    Re-adding Dataflow v1. (#1725)
    * Re-adding Dataflow v1.
    A user is having technical difficulties with the Beam API, so re-adding `bigtable-hbase-dataflow`.  Hopefully, this can be removed towards the end of 2018.
    
    * Dataflow is now actually built.  Removing old test: NettyRecyclerIsOffTes.

---
+++ bigtable-dataflow-parent/bigtable-hbase-dataflow/src/test/java/com/google/cloud/bigtable/dataflow/coders/HBaseMutationCoderTest.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (C) 2015 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.google.cloud.bigtable.dataflow.coders;
+
+import static org.apache.hadoop.hbase.util.Bytes.toBytes;
+
+import com.google.cloud.dataflow.sdk.util.MutationDetector;
+import com.google.cloud.dataflow.sdk.util.MutationDetectors;
+import java.io.IOException;
+import org.apache.hadoop.hbase.client.Delete;
+import org.apache.hadoop.hbase.client.Put;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Tests for {@link HBaseMutationCoder}.
+ */
+public class HBaseMutationCoderTest {
+
+  private HBaseMutationCoder underTest;
+
+  @Before
+  public void setup() {
+    underTest = new HBaseMutationCoder();
+  }
+
+  @Test
+  public void testPut() throws IOException {
+    Put original =
+        new Put(toBytes("key")).addColumn(toBytes("family"), toBytes("column"), toBytes("value"));
+    MutationDetector mutationDetector = MutationDetectors.forValueWithCoder(original, underTest);
+    for (int i = 0; i < 5; i++) {
+      Assert.assertEquals(
+          0, original.compareTo(CoderTestUtil.encodeAndDecode(underTest, original)));
+      Assert.assertEquals(
+          0, original.compareTo(CoderTestUtil.encodeAndDecode(underTest, original)));
+
+      // Make sure that the clock change didn't modify the serialized value.
+      mutationDetector.verifyUnmodified();
+    }
+  }
+
+  @Test
+  public void testDelete() throws IOException {
+    Delete original = new Delete(toBytes("key"));
+    MutationDetector mutationDetector = MutationDetectors.forValueWithCoder(original, underTest);
+    for (int i = 0; i < 5; i++) {
+      Assert.assertEquals(
+          0, original.compareTo(CoderTestUtil.encodeAndDecode(underTest, original)));
+
+      // Make sure that the clock change didn't modify the serialized value.
+      mutationDetector.verifyUnmodified();
+    }
+  }
+}
