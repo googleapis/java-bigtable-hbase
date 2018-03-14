7c2a129e8c4af20e3167fcdc313c978f5f01a6fb
Pawan Pullarwar <36055031+pawan-qlogic@users.noreply.github.com>
2018-03-14 22:13:09 +0530

    Added FuzzyRowFilter test case (#1731)

--- bigtable-hbase-parent/bigtable-hbase-1.x-integration-tests/src/test/java/com/google/cloud/bigtable/hbase/TestFilters.java
+++ bigtable-hbase-parent/bigtable-hbase-1.x-integration-tests/src/test/java/com/google/cloud/bigtable/hbase/TestFilters.java
@@ -17,6 +17,7 @@ package com.google.cloud.bigtable.hbase;
 
 import static com.google.cloud.bigtable.hbase.test_env.SharedTestEnvRule.COLUMN_FAMILY;
 import static com.google.cloud.bigtable.hbase.test_env.SharedTestEnvRule.COLUMN_FAMILY2;
+import static org.junit.Assert.assertEquals;
 
 import com.google.bigtable.repackaged.com.google.cloud.bigtable.data.v2.wrappers.Filters;
 import com.google.bigtable.repackaged.com.google.protobuf.ByteString;
@@ -77,6 +78,7 @@ import org.junit.experimental.categories.Category;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 import java.util.TreeSet;
@@ -2063,5 +2065,54 @@ public class TestFilters extends AbstractTest {
     table.put(puts);
 
     return table;
+  }
+  
+  @Test
+  @Category(KnownGap.class)
+  public void run() throws Exception {
+    Table table = getDefaultTable();
+    List<byte[]> keys = Collections.unmodifiableList(
+        Arrays.asList(
+        createKey(1, 2, 3, 4, 5, 6), 
+        createKey(1, 9, 9, 4, 9, 9),
+        createKey(2, 3, 4, 5, 6, 7)));
+    
+    List<Put> puts = new ArrayList<>();
+    for(byte[] key : keys) {
+      puts.add(new Put(key).addColumn(SharedTestEnvRule.COLUMN_FAMILY, Bytes.toBytes(0), Bytes.toBytes(0)));
+    }
+    
+    table.put(puts);
+
+    // match keys with 1 in the first position and 4 in the 4th position
+    Pair<byte[], byte[]> fuzzyData = Pair.newPair(createKey(1, 0, 0, 4), createKey(0, 1, 1, 0));
+    FuzzyRowFilter filter = new FuzzyRowFilter(ImmutableList.of(fuzzyData));
+    Scan scan = new Scan();
+    scan.setFilter(filter);
+
+    ResultScanner scanner = table.getScanner(scan);
+    
+    String expected1 = Arrays.toString(parseKey(createKey(1, 2, 3, 4, 5, 6)));
+    String expected2 = Arrays.toString(parseKey(createKey(1, 9, 9, 4, 9, 9)));
+    Result r1 = scanner.next();
+    assertEquals(expected1, Arrays.toString(parseKey(r1.getRow())));
+    Result r2 = scanner.next();
+    assertEquals(expected2, Arrays.toString(parseKey(r2.getRow())));
+  }
+  
+  private byte[] createKey(int... values) {
+    byte[] bytes = new byte[4 * values.length];
+    for (int i = 0; i < values.length; i++) {
+      System.arraycopy(Bytes.toBytes(values[i]), 0, bytes, 4 * i, 4);
+    }
+    return bytes;
+  }
+
+  private int[] parseKey(byte[] key) {
+    int[] values = new int[key.length / 4];
+    for (int i = 0; i < values.length; i++) {
+      values[i] = Bytes.toInt(key, 4 * i);
+    }
+    return values;
   }
 }
