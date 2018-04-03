d69ecf5cff6bed45b14f47a22a9ee304a028095e
Solomon Duskis <sduskis@google.com>
2018-04-03 16:49:41 -0400

    IntegrationTests no longer is required for running tests (#1777)
    * IntegrationTests no longer is required for running tests
    Added a new TableCreator interface that would alleviate some of the problems in running individual tests.  This should work towards a solution for #1755.
    
    * removing SharedTestEnvRule.setInstance()

---
+++ bigtable-hbase-parent/bigtable-hbase-1.x-integration-tests/src/test/java/com/google/cloud/bigtable/hbase/test_env/TableCreatorImpl.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2018 Google LLC All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.cloud.bigtable.hbase.test_env;
+
+import org.apache.hadoop.hbase.HColumnDescriptor;
+import org.apache.hadoop.hbase.HTableDescriptor;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.Admin;
+
+import java.io.IOException;
+
+/**
+ * Creates a table for HBase 1.*.
+ */
+public class TableCreatorImpl implements TableCreator {
+  @Override
+  public void createTable(Admin admin, TableName tableName) throws IOException {
+    HColumnDescriptor hcd = new HColumnDescriptor(SharedTestEnvRule.COLUMN_FAMILY)
+        .setMaxVersions(SharedTestEnvRule.MAX_VERSIONS);
+    HColumnDescriptor family2 = new HColumnDescriptor(SharedTestEnvRule.COLUMN_FAMILY2)
+        .setMaxVersions(SharedTestEnvRule.MAX_VERSIONS);
+    admin.createTable(new HTableDescriptor(tableName).addFamily(hcd).addFamily(family2));
+  }
+}
