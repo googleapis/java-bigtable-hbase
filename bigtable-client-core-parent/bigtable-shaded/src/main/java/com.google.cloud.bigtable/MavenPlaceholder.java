a6a454d757713a106379f622527d0dece5e427de
Ajay <ajay@qlogic.io>
2018-08-30 15:59:37 -0400

    Deprecate Filters related class instead of replace

---
+++ bigtable-client-core-parent/bigtable-shaded/src/main/java/com.google.cloud.bigtable/MavenPlaceholder.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2015 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.cloud.bigtable;
+
+/**
+ * This class is here to force generation of source javadoc jars so that the maven release process
+ * doesn't complain. The shading plugin generated a shaded jar of bigtable-hbase, but it doesn't
+ * generate javadoc or source files; this class is here as a hack and better methods should be
+ * employed.
+ *
+ * @author sduskis
+ * @version $Id: $Id
+ */
+public final class MavenPlaceholder {
+
+  private MavenPlaceholder() {
+
+  }
+}
