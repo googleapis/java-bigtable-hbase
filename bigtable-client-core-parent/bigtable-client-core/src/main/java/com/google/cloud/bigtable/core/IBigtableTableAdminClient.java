358516a224dd52647258814aa8c890bd734b1240
Rahul Kesharawni <rahulk@qlogic.io>
2019-01-04 21:36:31 +0530

    adding adminClient
    Add review comments

--- bigtable-client-core-parent/bigtable-client-core/src/main/java/com/google/cloud/bigtable/core/IBigtableTableAdminClient.java
+++ bigtable-client-core-parent/bigtable-client-core/src/main/java/com/google/cloud/bigtable/core/IBigtableTableAdminClient.java
@@ -1,41 +1,121 @@
+/*
+ * Copyright 2018 Google LLC. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.google.cloud.bigtable.core;
 
-import com.google.api.core.ApiFuture;
 import com.google.cloud.bigtable.admin.v2.models.CreateTableRequest;
 import com.google.cloud.bigtable.admin.v2.models.ModifyColumnFamiliesRequest;
 import com.google.cloud.bigtable.admin.v2.models.Table;
+import com.google.common.util.concurrent.ListenableFuture;
 import java.util.List;
 
 /**
- * Interface to access Bigtable data service api.
+ * Interface to access Bigtable gax table admin API.
  */
 public interface IBigtableTableAdminClient {
 
+  /**
+   * Creates a new table. The table can be created with a full set of initial column families,
+   * specified in the request.
+   * @param request a {@link CreateTableRequest} object.
+   */
   Table createTable(CreateTableRequest request);
 
-  ApiFuture<Table> createTableAsync(CreateTableRequest request);
+  /**
+   * Creates a new table asynchronously. The table can be created with a full set of initial column
+   * families, specified in the request.
+   *
+   * @param request a {@link CreateTableRequest} object.
+   */
+  ListenableFuture<Table> createTableAsync(CreateTableRequest request);
 
-  Table modifyFamilies(ModifyColumnFamiliesRequest request);
-
-  ApiFuture<Table> modifyFamiliesAsync(ModifyColumnFamiliesRequest request);
-
-  void deleteTable(String tableId);
-
-  ApiFuture<Void> deleteTableAsync(String tableId);
-
-  boolean exists(String tableId);
-
-  ApiFuture<Boolean> existsAsync(String tableId);
-
+  /**
+   * Gets the details of a table.
+   *
+   * @param tableId a String object.
+   * @return a {@link Table} object.
+   */
   Table getTable(String tableId);
 
-  ApiFuture<Table> getTableAsync(String tableId);
+  /**
+   * Gets the details of a table asynchronously.
+   *
+   * @return a {@link ListenableFuture} that returns a {@link Table} object.
+   */
+  ListenableFuture<Table> getTableAsync(String tableId);
 
+  /**
+   * Lists the names of all tables in an instance.
+   *
+   * @return an immutable {@link List} object containing tableId.
+   */
   List<String> listTables();
 
-  ApiFuture<List<String>> listTablesAsync();
+  /**
+   * Lists the names of all tables in an instance asynchronously.
+   *
+   * @return a {@link ListenableFuture} of type {@link Void} will be set when request is
+   *   successful otherwise exception will be thrown.
+   */
+  ListenableFuture<List<String>> listTablesAsync();
 
+  /**
+   * Permanently deletes a specified table and all of its data.
+   *
+   */
+  void deleteTable(String tableId);
+
+  /**
+   * Permanently deletes a specified table and all of its data.
+   *
+   * @return a {@link ListenableFuture} of type {@link Void} will be set when request is
+   *  successful otherwise exception will be thrown.
+   */
+  ListenableFuture<Void> deleteTableAsync(String tableId);
+
+  /**
+   * Creates, modifies or deletes a new column family within a specified table.
+   *
+   * @param request a {@link ModifyColumnFamiliesRequest} object.
+   * @return return {@link Table} object  that contains the updated table structure.
+   */
+  Table modifyFamilies(ModifyColumnFamiliesRequest request);
+
+  /**
+   * Creates, modifies or deletes a new column family within a specified table.
+   *
+   * @param request a {@link ModifyColumnFamiliesRequest} object.
+   * @return a {@link ListenableFuture} that returns {@link Table} object that contains
+   *  the updated table structure.
+   */
+  ListenableFuture<Table> modifyFamiliesAsync(ModifyColumnFamiliesRequest request);
+
+  /**
+   * Permanently deletes all rows in a range.
+   *
+   * @param tableId
+   * @param rowKeyPrefix
+   */
   void dropRowRange(String tableId, String rowKeyPrefix);
 
-  ApiFuture<Void> dropRowRangeAsync(String tableId, String rowKeyPrefix);
+  /**
+   * Permanently deletes all rows in a range.
+   *
+   * @param tableId
+   * @param rowKeyPrefix
+   * @return a {@link ListenableFuture} that returns {@link Void} object.
+   */
+  ListenableFuture<Void> dropRowRangeAsync(String tableId, String rowKeyPrefix);
 }
