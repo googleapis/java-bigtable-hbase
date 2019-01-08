c00931ddfdd76150caca22aaacc5e76c49f361b5
Rahul Kesharwani <42969463+rahulKQL@users.noreply.github.com>
2019-01-08 21:33:19 +0530

    Admin interface as GAX admin client (#2040)
    * adding first interface
    
    * adding adminClient
    
    * adding first interface
    
    * adding adminClient
    
    Add review comments
    
    * added todo comments
    
    * updated copyright year & remove whitespace
    
    * worked on feedback

---
+++ bigtable-client-core-parent/bigtable-client-core/src/main/java/com/google/cloud/bigtable/grpc/BigtableTableAdminClientWrapper.java
@@ -0,0 +1,223 @@
+/*
+ * Copyright 2019 Google LLC. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.cloud.bigtable.grpc;
+
+import com.google.bigtable.admin.v2.DeleteTableRequest;
+import com.google.bigtable.admin.v2.DropRowRangeRequest;
+import com.google.bigtable.admin.v2.GetTableRequest;
+import com.google.bigtable.admin.v2.ListTablesRequest;
+import com.google.bigtable.admin.v2.ListTablesResponse;
+import com.google.cloud.bigtable.admin.v2.models.CreateTableRequest;
+import com.google.cloud.bigtable.admin.v2.models.ModifyColumnFamiliesRequest;
+import com.google.cloud.bigtable.admin.v2.models.Table;
+import com.google.cloud.bigtable.config.BigtableOptions;
+import com.google.cloud.bigtable.core.IBigtableTableAdminClient;
+import com.google.common.base.Function;
+import com.google.common.base.Preconditions;
+import com.google.common.collect.ImmutableList;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.MoreExecutors;
+import com.google.protobuf.ByteString;
+import com.google.protobuf.Empty;
+import java.util.List;
+import javax.annotation.Nonnull;
+
+/**
+ * This class implements the {@link IBigtableTableAdminClient} interface and wraps
+ * {@link BigtableTableAdminClient} with Google-cloud-java's models.
+ */
+public class BigtableTableAdminClientWrapper implements IBigtableTableAdminClient {
+
+  private final BigtableTableAdminClient adminClient;
+  private final BigtableInstanceName instanceName;
+
+  public BigtableTableAdminClientWrapper(@Nonnull BigtableTableAdminClient adminClient,
+      @Nonnull BigtableOptions options){
+    Preconditions.checkNotNull(adminClient);
+    Preconditions.checkNotNull(options);
+    this.adminClient = adminClient;
+    this.instanceName = new BigtableInstanceName(options.getProjectId(),
+        options.getInstanceId());
+  }
+
+  /** {@inheritDoc} */
+  @Override
+  public Table createTable(CreateTableRequest request) {
+    com.google.bigtable.admin.v2.CreateTableRequest requestProto =
+        request.toProto(instanceName.toAdminInstanceName());
+    adminClient.createTable(requestProto);
+
+    return getTable(requestProto.getTableId());
+  }
+
+  /** {@inheritDoc} */
+  @Override
+  public ListenableFuture<Table> createTableAsync(CreateTableRequest request) {
+    com.google.bigtable.admin.v2.CreateTableRequest requestProto =
+        request.toProto(instanceName.toAdminInstanceName());
+
+    return Futures.transform(adminClient.createTableAsync(requestProto),
+        new Function<com.google.bigtable.admin.v2.Table, Table>() {
+          @Override
+          public Table apply(com.google.bigtable.admin.v2.Table tableProto) {
+            return Table.fromProto(tableProto);
+          }
+        }, MoreExecutors.directExecutor());
+  }
+
+  /** {@inheritDoc} */
+  @Override
+  public Table getTable(String tableId) {
+    GetTableRequest requestProto = GetTableRequest.newBuilder()
+        .setName(instanceName.toTableNameStr(tableId))
+        .build();
+
+    return Table.fromProto(adminClient.getTable(requestProto));
+  }
+
+  /** {@inheritDoc} */
+  @Override
+  public ListenableFuture<Table> getTableAsync(String tableId) {
+    GetTableRequest requestProto = GetTableRequest.newBuilder()
+        .setName(instanceName.toTableNameStr(tableId))
+        .build();
+
+    return Futures.transform(adminClient.getTableAsync(requestProto),
+        new Function<com.google.bigtable.admin.v2.Table, Table>() {
+          @Override
+          public Table apply(com.google.bigtable.admin.v2.Table tableProto) {
+            return Table.fromProto(tableProto);
+          }
+        }, MoreExecutors.directExecutor());
+  }
+
+  /** {@inheritDoc} */
+  @Override
+  public List<String> listTables() {
+    ListTablesRequest requestProto = ListTablesRequest.newBuilder()
+        .setParent(instanceName.toString())
+        .build();
+
+    ListTablesResponse response = adminClient.listTables(requestProto);
+
+    ImmutableList.Builder<String> tableIdsBuilder =
+        ImmutableList.builderWithExpectedSize(response.getTablesList().size());
+    for(com.google.bigtable.admin.v2.Table tableProto : response.getTablesList()){
+      tableIdsBuilder.add(instanceName.toTableId(tableProto.getName()));
+    }
+
+    return tableIdsBuilder.build();
+  }
+
+  /** {@inheritDoc} */
+  @Override
+  public ListenableFuture<List<String>> listTablesAsync() {
+    ListTablesRequest request = ListTablesRequest.newBuilder()
+        .setParent(instanceName.toString())
+        .build();
+    ListenableFuture<ListTablesResponse> response = adminClient.listTablesAsync(request);
+
+    return Futures.transform(response, new Function<ListTablesResponse, List<String>>() {
+      @Override
+      public List<String> apply(ListTablesResponse input) {
+        ImmutableList.Builder<String> tableIdsBuilder =
+            ImmutableList.builderWithExpectedSize(input.getTablesList().size());
+        for(com.google.bigtable.admin.v2.Table tableProto : input.getTablesList()){
+          tableIdsBuilder.add(instanceName.toTableId(tableProto.getName()));
+        }
+
+        return tableIdsBuilder.build();
+      }
+    }, MoreExecutors.directExecutor());
+  }
+
+  /** {@inheritDoc} */
+  @Override
+  public void deleteTable(String tableId) {
+    DeleteTableRequest request = DeleteTableRequest.newBuilder()
+        .setName(instanceName.toTableNameStr(tableId))
+        .build();
+
+    adminClient.deleteTable(request);
+  }
+
+  /** {@inheritDoc} */
+  @Override
+  public ListenableFuture<Void> deleteTableAsync(String tableId) {
+    DeleteTableRequest request = DeleteTableRequest.newBuilder()
+        .setName(instanceName.toTableNameStr(tableId))
+        .build();
+
+    return Futures.transform(adminClient.deleteTableAsync(request), new Function<Empty, Void>() {
+      @Override
+      public Void apply(Empty empty) {
+        return null;
+      }
+    }, MoreExecutors.directExecutor());
+  }
+
+  /** {@inheritDoc} */
+  @Override
+  public Table modifyFamilies(ModifyColumnFamiliesRequest request) {
+    com.google.bigtable.admin.v2.ModifyColumnFamiliesRequest modifyColumnRequestProto =
+        request.toProto(instanceName.toAdminInstanceName());
+
+    return Table.fromProto(adminClient.modifyColumnFamily(modifyColumnRequestProto));
+  }
+
+  /** {@inheritDoc} */
+  @Override
+  public ListenableFuture<Table> modifyFamiliesAsync(ModifyColumnFamiliesRequest request) {
+    com.google.bigtable.admin.v2.ModifyColumnFamiliesRequest modifyColumnRequestProto =
+        request.toProto(instanceName.toAdminInstanceName());
+
+    return Futures.transform(adminClient.modifyColumnFamilyAsync(modifyColumnRequestProto),
+        new Function<com.google.bigtable.admin.v2.Table, Table>() {
+      @Override
+      public Table apply(com.google.bigtable.admin.v2.Table tableProto) {
+        return Table.fromProto(tableProto);
+      }
+    }, MoreExecutors.directExecutor());
+  }
+
+  /** {@inheritDoc} */
+  @Override
+  public void dropRowRange(String tableId, String rowKeyPrefix) {
+    DropRowRangeRequest requestProto = DropRowRangeRequest.newBuilder()
+        .setName(instanceName.toTableNameStr(tableId))
+        .setRowKeyPrefix(ByteString.copyFromUtf8(rowKeyPrefix))
+        .build();
+
+    adminClient.dropRowRange(requestProto);
+  }
+
+  /** {@inheritDoc} */
+  @Override
+  public ListenableFuture<Void> dropRowRangeAsync(String tableId, String rowKeyPrefix) {
+    DropRowRangeRequest requestProto = DropRowRangeRequest.newBuilder()
+        .setName(instanceName.toTableNameStr(tableId))
+        .setRowKeyPrefix(ByteString.copyFromUtf8(rowKeyPrefix))
+        .build();
+
+    return Futures.transform(adminClient.dropRowRangeAsync(requestProto), new Function<Empty, Void>() {
+      @Override
+      public Void apply(Empty empty) {
+          return null;
+      }
+    }, MoreExecutors.directExecutor());
+  }
+}
---
+++ bigtable-client-core-parent/bigtable-client-core/src/test/java/com/google/cloud/bigtable/grpc/TestBigtableTableAdminClientWrapper.java
@@ -0,0 +1,255 @@
+/*
+ * Copyright 2019 Google LLC. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.cloud.bigtable.grpc;
+
+import static com.google.cloud.bigtable.admin.v2.models.GCRules.GCRULES;
+import static com.google.common.util.concurrent.Futures.immediateFuture;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import com.google.bigtable.admin.v2.ColumnFamily;
+import com.google.bigtable.admin.v2.DeleteTableRequest;
+import com.google.bigtable.admin.v2.DropRowRangeRequest;
+import com.google.bigtable.admin.v2.GetTableRequest;
+import com.google.bigtable.admin.v2.ListTablesRequest;
+import com.google.bigtable.admin.v2.ListTablesResponse;
+import com.google.cloud.bigtable.admin.v2.models.CreateTableRequest;
+import com.google.cloud.bigtable.admin.v2.models.GCRules;
+import com.google.cloud.bigtable.admin.v2.models.ModifyColumnFamiliesRequest;
+import com.google.cloud.bigtable.admin.v2.models.Table;
+import com.google.cloud.bigtable.config.BigtableOptions;
+import com.google.common.collect.ImmutableList;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.protobuf.ByteString;
+import com.google.protobuf.Empty;
+import java.util.List;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.mockito.Mockito;
+
+@RunWith(JUnit4.class)
+public class TestBigtableTableAdminClientWrapper {
+  private static final String PROJECT_ID = "projectId";
+  private static final String INSTANCE_ID = "instanceId";
+  private static final String TABLE_ID = "tableId";
+  private static final String APP_PROFILE_ID = "appProfileId";
+  private static final String COLUMN_FAMILY = "myColumnFamily";
+  private static final String ROW_KEY_PREFIX = "row-key-val";
+  private static final String UPDATE_FAMILY = "update-family";
+  private static final String TEST_TABLE_ID_1 = "test-table-1";
+  private static final String TEST_TABLE_ID_2 = "test-table-2";
+  private static final String TEST_TABLE_ID_3 = "test-table-3";
+
+  private BigtableOptions options = BigtableOptions.builder()
+      .setProjectId(PROJECT_ID)
+      .setInstanceId(INSTANCE_ID)
+      .setAppProfileId(APP_PROFILE_ID)
+      .build();
+  private static final BigtableInstanceName INSTANCE_NAME = new BigtableInstanceName(PROJECT_ID,
+      INSTANCE_ID);
+  private static final String TABLE_NAME = INSTANCE_NAME.toTableNameStr(TABLE_ID);
+
+  private BigtableTableAdminClient mockAdminClient;
+
+  private BigtableTableAdminClientWrapper clientWrapper;
+
+
+  @Before
+  public void setUp(){
+    mockAdminClient = Mockito.mock(BigtableTableAdminClient.class);
+    clientWrapper = new BigtableTableAdminClientWrapper(mockAdminClient, options);
+  }
+
+  @Test
+  public void testCreateTable(){
+    CreateTableRequest request = CreateTableRequest.of(TABLE_ID);
+    GetTableRequest getTableRequest = GetTableRequest.newBuilder().setName(TABLE_NAME).build();
+
+    doNothing().when(mockAdminClient).createTable(request.toProto(INSTANCE_NAME.toAdminInstanceName()));
+    when(mockAdminClient.getTable(getTableRequest)).thenReturn(createTableData());
+    Table response = clientWrapper.createTable(request);
+
+    assertEquals(Table.fromProto(createTableData()), response);
+    verify(mockAdminClient).createTable(request.toProto(INSTANCE_NAME.toAdminInstanceName()));
+    verify(mockAdminClient).getTable(getTableRequest);
+  }
+
+  @Test
+  public void testCreateTableAsync() throws Exception{
+    CreateTableRequest request = CreateTableRequest.of(TABLE_ID);
+
+    when(mockAdminClient.createTableAsync(request.toProto(INSTANCE_NAME.toAdminInstanceName())))
+        .thenReturn(immediateFuture(createTableData()));
+    ListenableFuture<Table> response = clientWrapper.createTableAsync(request);
+
+    assertEquals(Table.fromProto(createTableData()), response.get());
+    verify(mockAdminClient).createTableAsync(request.toProto(INSTANCE_NAME.toAdminInstanceName()));
+  }
+
+  @Test
+  public void testGetTable(){
+    GetTableRequest request = GetTableRequest.newBuilder().setName(TABLE_NAME).build();
+
+    when(mockAdminClient.getTable(request)).thenReturn(createTableData());
+    Table response = clientWrapper.getTable(TABLE_ID);
+
+    assertEquals(Table.fromProto(createTableData()), response);
+    verify(mockAdminClient).getTable(request);
+  }
+
+  @Test
+  public void testGetTableAsync() throws Exception{
+    GetTableRequest request = GetTableRequest.newBuilder().setName(TABLE_NAME).build();
+
+    when(mockAdminClient.getTableAsync(request))
+        .thenReturn(immediateFuture(createTableData()));
+    ListenableFuture<Table> response = clientWrapper.getTableAsync(TABLE_ID);
+
+    assertEquals(Table.fromProto(createTableData()), response.get());
+    verify(mockAdminClient).getTableAsync(request);
+  }
+
+  @Test
+  public void testListTables(){
+    ImmutableList<String> tableIdList = ImmutableList.of(TEST_TABLE_ID_1, TEST_TABLE_ID_2,
+        TEST_TABLE_ID_3);
+    ListTablesRequest request =
+        ListTablesRequest.newBuilder().setParent(INSTANCE_NAME.toString()).build();
+    ListTablesResponse.Builder builder = ListTablesResponse.newBuilder();
+    builder.addTablesBuilder().setName(INSTANCE_NAME.toTableNameStr(TEST_TABLE_ID_1)).build();
+    builder.addTablesBuilder().setName(INSTANCE_NAME.toTableNameStr(TEST_TABLE_ID_2)).build();
+    builder.addTablesBuilder().setName(INSTANCE_NAME.toTableNameStr(TEST_TABLE_ID_3)).build();
+
+    when(mockAdminClient.listTables(request)).thenReturn(builder.build());
+    List<String> actualResponse = clientWrapper.listTables();
+
+    assertEquals(tableIdList, actualResponse);
+    verify(mockAdminClient).listTables(request);
+  }
+
+  @Test
+  public void testListTablesAsync() throws Exception{
+    ImmutableList<String> tableIdList = ImmutableList.of(TEST_TABLE_ID_1, TEST_TABLE_ID_2,
+        TEST_TABLE_ID_3);
+    ListTablesRequest request =
+        ListTablesRequest.newBuilder().setParent(INSTANCE_NAME.toString()).build();
+    ListTablesResponse.Builder builder = ListTablesResponse.newBuilder();
+    builder.addTablesBuilder().setName(INSTANCE_NAME.toTableNameStr(TEST_TABLE_ID_1)).build();
+    builder.addTablesBuilder().setName(INSTANCE_NAME.toTableNameStr(TEST_TABLE_ID_2)).build();
+    builder.addTablesBuilder().setName(INSTANCE_NAME.toTableNameStr(TEST_TABLE_ID_3)).build();
+
+    when(mockAdminClient.listTablesAsync(request)).thenReturn(immediateFuture(builder.build()));
+    ListenableFuture<List<String>> actualResponse = clientWrapper.listTablesAsync();
+
+    assertEquals(tableIdList, actualResponse.get());
+    verify(mockAdminClient).listTablesAsync(request);
+  }
+
+  @Test
+  public void testDeleteTable(){
+    DeleteTableRequest request = DeleteTableRequest.newBuilder().setName(TABLE_NAME).build();
+
+    doNothing().when(mockAdminClient).deleteTable(request);
+    clientWrapper.deleteTable(TABLE_ID);
+
+    verify(mockAdminClient).deleteTable(request);
+  }
+
+  @Test
+  public void testDeleteTableAsync(){
+    DeleteTableRequest request = DeleteTableRequest.newBuilder().setName(TABLE_NAME).build();
+
+    when(mockAdminClient.deleteTableAsync(request))
+        .thenReturn(immediateFuture(Empty.newBuilder().build()));
+    clientWrapper.deleteTableAsync(TABLE_ID);
+
+    verify(mockAdminClient).deleteTableAsync(request);
+  }
+
+  @Test
+  public void testModifyFamilies(){
+    ModifyColumnFamiliesRequest request =
+        ModifyColumnFamiliesRequest
+            .of(TABLE_ID)
+            .addFamily(COLUMN_FAMILY, GCRULES.maxVersions(1))
+            .updateFamily(UPDATE_FAMILY, GCRULES.defaultRule());
+
+    when(mockAdminClient.modifyColumnFamily(request.toProto(INSTANCE_NAME.toAdminInstanceName())))
+        .thenReturn(createTableData());
+    Table response = clientWrapper.modifyFamilies(request);
+
+    assertEquals(Table.fromProto(createTableData()), response);
+    verify(mockAdminClient).modifyColumnFamily(request.toProto(INSTANCE_NAME.toAdminInstanceName()));
+  }
+
+  @Test
+  public void testModifyFamiliesAsync() throws Exception{
+    ModifyColumnFamiliesRequest request =
+        ModifyColumnFamiliesRequest
+            .of(TABLE_ID)
+            .addFamily(COLUMN_FAMILY, GCRULES.maxVersions(1))
+            .updateFamily(UPDATE_FAMILY, GCRULES.defaultRule());
+
+    when(mockAdminClient.modifyColumnFamilyAsync(request.toProto(INSTANCE_NAME.toAdminInstanceName())))
+        .thenReturn(immediateFuture(createTableData()));
+    ListenableFuture<Table> response = clientWrapper.modifyFamiliesAsync(request);
+
+    assertEquals(Table.fromProto(createTableData()), response.get());
+    verify(mockAdminClient).modifyColumnFamilyAsync(request.toProto(INSTANCE_NAME.toAdminInstanceName()));
+  }
+
+  @Test
+  public void dropRowRange(){
+    DropRowRangeRequest request = DropRowRangeRequest.newBuilder()
+        .setName(TABLE_NAME)
+        .setRowKeyPrefix(ByteString.copyFromUtf8(ROW_KEY_PREFIX))
+        .build();
+
+    doNothing().when(mockAdminClient).dropRowRange(request);
+    clientWrapper.dropRowRange(TABLE_ID, ROW_KEY_PREFIX);
+
+    verify(mockAdminClient).dropRowRange(request);
+  }
+
+  @Test
+  public void dropRowRangeAsync(){
+    DropRowRangeRequest request = DropRowRangeRequest.newBuilder()
+        .setName(TABLE_NAME)
+        .setRowKeyPrefix(ByteString.copyFromUtf8(ROW_KEY_PREFIX))
+        .build();
+
+    when(mockAdminClient.dropRowRangeAsync(request))
+        .thenReturn(immediateFuture(Empty.newBuilder().build()));
+    clientWrapper.dropRowRangeAsync(TABLE_ID, ROW_KEY_PREFIX);
+
+    verify(mockAdminClient).dropRowRangeAsync(request);
+  }
+
+  private static com.google.bigtable.admin.v2.Table createTableData(){
+    GCRules.GCRule gcRule = GCRULES.maxVersions(1);
+    ColumnFamily columnFamily = ColumnFamily.newBuilder()
+        .setGcRule(gcRule.toProto()).build();
+
+    return com.google.bigtable.admin.v2.Table.newBuilder()
+        .setName(TABLE_NAME)
+        .putColumnFamilies(COLUMN_FAMILY, columnFamily)
+        .build();
+  }
+}
