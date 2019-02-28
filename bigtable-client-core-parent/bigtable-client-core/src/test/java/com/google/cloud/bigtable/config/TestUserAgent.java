0d751aeb32ff3446f242515a29b009560181acaa
Rahul Kesharwani <42969463+rahulKQL@users.noreply.github.com>
2019-02-28 19:54:25 +0530

    Data settings addition (#1973)
    * static method for BigtableDataSettings creation
    
    only adding fromBigtableOptions static method
    
    adding separate methods for each settings
    
    Added check for retry & remvoed unneeded setter
    
    * Adding BigtableDataClient & BigtableAdminClient
    
    renamed Factory methods
    
    Reverted BulkMutationSettings changes
    
    refactored Credentail Provider
    
    * Added User-Agent test along with more feedback changes
    
    * added ChannelPerCPU config
    
    * Adding Refactored BigtableVeneerSettings
    
    * Rebase & Refactored retryStatusCodes
    
    * Addressing Feedback for BigtableVeneerSettings
    
    * Update BigtableVeneerSettingsFactory.java
    
    Used constant for maxRetryDelay & explicitly set the maxAttempts to zero
    
    * Updated Veneer Settings
    
    - Now using EnhancedBigtableDataSettings.defaultGrpcChannelPRovider for channel provider configuration.
    - Updated existed BigtableDataSetttings.Builder to EnhancedBigtableDataSettings.Builder.
    - changed user agent for vaneer adapter to lowercase.
    - throwing exception in case of allowRetriesWithoutTimestamp.

---
+++ bigtable-client-core-parent/bigtable-client-core/src/test/java/com/google/cloud/bigtable/config/TestUserAgent.java
@@ -0,0 +1,237 @@
+/*
+ * Copyright 2019 Google LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.cloud.bigtable.config;
+
+import com.google.api.core.ApiFunction;
+import com.google.api.gax.core.NoCredentialsProvider;
+import com.google.api.gax.grpc.InstantiatingGrpcChannelProvider;
+import com.google.api.gax.rpc.FixedHeaderProvider;
+import com.google.api.gax.rpc.HeaderProvider;
+import com.google.bigtable.v2.BigtableGrpc.BigtableImplBase;
+import com.google.bigtable.v2.ReadRowsRequest;
+import com.google.bigtable.v2.ReadRowsResponse;
+import com.google.cloud.bigtable.data.v2.BigtableDataClient;
+import com.google.cloud.bigtable.data.v2.BigtableDataSettings;
+import com.google.common.io.Resources;
+import io.grpc.ForwardingServerCall;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.Metadata;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import io.grpc.ServerCall;
+import io.grpc.ServerCallHandler;
+import io.grpc.ServerInterceptor;
+import io.grpc.ServerInterceptors;
+import io.grpc.netty.shaded.io.grpc.netty.GrpcSslContexts;
+import io.grpc.netty.shaded.io.grpc.netty.NettyChannelBuilder;
+import io.grpc.netty.shaded.io.netty.handler.ssl.SslContext;
+import io.grpc.netty.shaded.io.netty.handler.ssl.SslContextBuilder;
+import io.grpc.stub.StreamObserver;
+import java.io.File;
+import java.net.ServerSocket;
+import java.net.URL;
+import java.util.regex.Pattern;
+import javax.net.ssl.SSLException;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+import static io.grpc.internal.GrpcUtil.USER_AGENT_KEY;
+
+/**
+ * This class tests value present in User-Agent's on netty server.
+ */
+@RunWith(JUnit4.class)
+public class TestUserAgent {
+
+  private static final Logger logger = new Logger(TestUserAgent.class);
+
+  private static final String TEST_PROJECT_ID = "ProjectId";
+  private static final String TEST_INSTANCE_ID = "InstanceId";
+  private static final String TEST_USER_AGENT = "test-user-agent";
+  private static final Pattern EXPECTED_HEADER_PATTERN =
+      Pattern.compile(".*" + TEST_USER_AGENT + ".*");
+  private static final String TABLE_ID = "my-table-id";
+  private static final String ROWKEY = "row-key";
+
+  private BigtableDataSettings dataSettings;
+  private BigtableDataClient dataClient;
+  private Server server;
+
+  @After
+  public void tearDown() throws Exception {
+    if(dataClient != null){
+      dataClient.close();
+    }
+    if (server != null) {
+      server.shutdown();
+      server.awaitTermination();
+    }
+  }
+
+  /**
+   * To Test UserAgent & PlainText Negotiation type
+   * when {@link BigtableDataSettings} is created using {@link BigtableOptions}.
+   */
+  @Test
+  public void testUserAgentUsingPlainTextNegotiation() throws Exception{
+    ServerSocket serverSocket = new ServerSocket(0);
+    final int availablePort = serverSocket.getLocalPort();
+    serverSocket.close();
+
+    //Creates non-ssl server.
+    createServer(availablePort);
+
+    BigtableOptions bigtableOptions =
+        BigtableOptions.builder()
+            .setDataHost("localhost")
+            .setAdminHost("localhost")
+            .setProjectId(TEST_PROJECT_ID)
+            .setInstanceId(TEST_INSTANCE_ID)
+            .setUserAgent(TEST_USER_AGENT)
+            .setUsePlaintextNegotiation(true)
+            .setCredentialOptions(CredentialOptions.nullCredential())
+            .setPort(availablePort)
+            .build();
+
+    dataSettings = BigtableVeneerSettingsFactory.createBigtableDataSettings(bigtableOptions);
+
+    dataClient = BigtableDataClient.create(dataSettings);
+    dataClient.readRow(TABLE_ID, ROWKEY);
+  }
+
+  /**
+   * Verify userAgent on TLS Negotiation with {@link InstantiatingGrpcChannelProvider} using SSL
+   * enabled server.
+   */
+  @Test
+  public void testUserAgentUsingTLSNegotiation() throws Exception {
+    ServerSocket serverSocket = new ServerSocket(0);
+    final int availablePort = serverSocket.getLocalPort();
+    serverSocket.close();
+
+    //Creates SSL enabled server.
+    createSecuredServer(availablePort);
+
+    BigtableDataSettings.Builder builder =
+        BigtableDataSettings.newBuilder()
+            .setProjectId(TEST_PROJECT_ID)
+            .setInstanceId(TEST_INSTANCE_ID)
+            .setCredentialsProvider(NoCredentialsProvider.create());
+
+    //Loads secured Certificate
+    final SslContext sslContext  = buildSslContext();
+
+    final String endpoint = "localhost" + ":" + availablePort;
+    HeaderProvider headers = FixedHeaderProvider.create(USER_AGENT_KEY.name(), TEST_USER_AGENT);
+
+    builder.setTransportChannelProvider(InstantiatingGrpcChannelProvider.newBuilder()
+        .setHeaderProvider(headers)
+        .setEndpoint(endpoint)
+        .setChannelConfigurator(new ApiFunction<ManagedChannelBuilder, ManagedChannelBuilder>() {
+          @Override
+          public ManagedChannelBuilder apply(ManagedChannelBuilder input) {
+            return ((NettyChannelBuilder)input).sslContext(sslContext);
+          }
+        })
+        .build());
+
+    dataClient = BigtableDataClient.create(builder.build());
+    dataClient.readRow(TABLE_ID, ROWKEY);
+  }
+
+  /** Creates simple server to intercept plainText Negotiation RPCs. */
+  private void createServer(int port) throws Exception{
+    server = ServerBuilder.forPort(port).addService(
+        ServerInterceptors.intercept(new BigtableExtendedImpl(), new HeaderServerInterceptor()))
+        .build();
+    server.start();
+  }
+
+  /** Creates secured server to intercept TLS Negotiation RPCs. */
+  private void createSecuredServer(int port) throws Exception {
+    ServerBuilder builder = ServerBuilder.forPort(port).addService(
+        ServerInterceptors.intercept(new BigtableExtendedImpl(), new HeaderServerInterceptor()));
+
+    try {
+      URL serverCertChain = Resources.getResource("sslCertificates/server_trust.crt");
+      URL privateKey = Resources.getResource("sslCertificates/server_key.pem");
+
+      builder.useTransportSecurity(new File(serverCertChain.getFile()),
+          new File(privateKey.getFile()));
+    } catch (Exception ex) {
+      throw new AssertionError("No server certificates found");
+    }
+    server = builder.build();
+    server.start();
+  }
+
+  /** Creates SSLContext from certificate & private key provided in classpath. */
+  private static SslContext buildSslContext() throws SSLException {
+    SslContextBuilder builder = GrpcSslContexts.forClient();
+
+    try {
+      URL url = Resources.getResource("sslCertificates/client_trust.crt");
+      builder.trustManager(new File(url.getFile()));
+    } catch (Exception ex) {
+      throw new AssertionError("No client trust certificate found");
+    }
+
+    return builder.build();
+  }
+
+  /**
+   * Overrides {@link BigtableImplBase#readRows(ReadRowsRequest, StreamObserver)} and returns
+   * dummy response.
+   * */
+  private static class BigtableExtendedImpl extends BigtableImplBase {
+    @Override
+    public void readRows(ReadRowsRequest request,
+        StreamObserver<ReadRowsResponse> responseObserver) {
+      responseObserver.onNext(ReadRowsResponse.getDefaultInstance());
+      responseObserver.onCompleted();
+    }
+  }
+
+  /**
+   * Asserts value of UserAgent header with EXPECTED_HEADER_PATTERN passed to the
+   * {@link InstantiatingGrpcChannelProvider}.
+   *
+   * Throws {@link AssertionError} when UserAgent's pattern does not match.
+   */
+  private class HeaderServerInterceptor implements ServerInterceptor {
+    @Override
+    public <ReqT, RespT> ServerCall.Listener<ReqT> interceptCall(ServerCall<ReqT, RespT> call,
+        final Metadata requestHeaders, ServerCallHandler<ReqT, RespT> next) {
+
+      //Logging all available headers.
+      logger.info("headers received from BigtableDataClient:" + requestHeaders);
+
+      Metadata.Key<String> USER_AGENT_KEY =
+          Metadata.Key.of("user-agent", Metadata.ASCII_STRING_MARSHALLER);
+      String headerValue = requestHeaders.get(USER_AGENT_KEY);
+
+      //In case of user-agent not matching, throwing AssertionError.
+      if (!EXPECTED_HEADER_PATTERN.matcher(headerValue).matches()) {
+        throw new AssertionError("User-Agent's format did not match");
+      }
+      return next.startCall(new ForwardingServerCall.SimpleForwardingServerCall<ReqT, RespT>(call) {
+      }, requestHeaders);
+    }
+  }
+}
+
