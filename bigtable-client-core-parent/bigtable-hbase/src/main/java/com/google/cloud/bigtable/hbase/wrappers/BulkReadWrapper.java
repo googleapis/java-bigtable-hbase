de7efc6cff7140be0210e63f5c52c1da04ec6f60
Rahul Kesharwani <rahulk@qlogic.io>
2020-03-11 20:42:51 +0530

    feat: adding client wrapper interfaces
    This change contains wrapper interfaces for Data, Admin, BulkMutation,
    BulkRead.

---
+++ bigtable-client-core-parent/bigtable-hbase/src/main/java/com/google/cloud/bigtable/hbase/wrappers/BulkReadWrapper.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2020 Google LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.cloud.bigtable.hbase.wrappers;
+
+import com.google.api.core.ApiFuture;
+import com.google.api.core.InternalApi;
+import com.google.cloud.bigtable.data.v2.models.Query;
+import java.io.IOException;
+import org.apache.hadoop.hbase.client.Result;
+
+/**
+ * Interface to read multiple rows in batched mode from a single table.
+ *
+ * <p>For internal use only - public for technical reasons.
+ */
+@InternalApi("For internal usage only")
+public interface BulkReadWrapper extends AutoCloseable {
+
+  /**
+   * Adds the key in the request to a batch read. The future will be resolved when the batch
+   * response is received.
+   */
+  ApiFuture<Result> add(Query query);
+
+  /**
+   * Sends all remaining requests to the server. This method does not wait for the method to
+   * complete.
+   */
+  void flush();
+
+  /** Closes the batch and prevents new keys addition. */
+  void close() throws IOException;
+}
