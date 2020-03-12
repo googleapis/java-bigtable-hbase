cd64248427222d7f7faa2088db3c1c751a56d56a
Rahul Kesharwani <42969463+rahulKQL@users.noreply.github.com>
2020-03-13 03:06:57 +0530

    chore: add version information utility (#2408)
    * chore: add version information utility
    
    This contains a utility to fetch current version info(Extracted from
    existing BigtableVersionInfo.java).
    
    * address feedback comment
     - added JavaDoc
     - updated property name
     - one unit test to verify version number
    
    * Made getVersion() as public method
    
    * updated test case to incorporate ComparisionChain

---
+++ bigtable-client-core-parent/bigtable-hbase/src/main/java/com/google/cloud/bigtable/hbase/BigtableHBaseVersion.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2020 Google LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.cloud.bigtable.hbase;
+
+import com.google.api.core.InternalApi;
+import com.google.cloud.bigtable.hbase.util.Logger;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+/**
+ * The version in this class is project build version specified in the maven. Maven reads the
+ * <strong>bigtable-hbase.properties</strong> file present in the resources directory and replaces
+ * the property placeholder with project version.
+ *
+ * <p>For internal use only - public for technical reasons.
+ */
+@InternalApi("For internal usage only")
+public class BigtableHBaseVersion {
+
+  private static final Logger LOG = new Logger(BigtableHBaseVersion.class);
+
+  /**
+   * Gets user agent from bigtable-hbase-version.properties. Returns a default dev user agent with
+   * current timestamp if not found.
+   */
+  public static String getVersion() {
+    final String defaultVersion = "dev-" + System.currentTimeMillis();
+    final String fileName = "bigtable-hbase-version.properties";
+    final String versionProperty = "bigtable-hbase.version";
+    try (InputStream stream = BigtableHBaseVersion.class.getResourceAsStream(fileName)) {
+      if (stream == null) {
+        LOG.error("Could not load properties file %s", fileName);
+        return defaultVersion;
+      }
+
+      Properties properties = new Properties();
+      properties.load(stream);
+      String value = properties.getProperty(versionProperty);
+      if (value == null) {
+        LOG.error("%s not found in %s.", versionProperty, fileName);
+      } else if (value.startsWith("$")) {
+        LOG.info("%s property is not replaced.", versionProperty);
+      } else {
+        return value;
+      }
+    } catch (IOException e) {
+      LOG.error("Error while trying to get user agent name from %s", e, fileName);
+    }
+    return defaultVersion;
+  }
+}
---
+++ bigtable-client-core-parent/bigtable-hbase/src/test/java/com/google/cloud/bigtable/hbase/TestBigtableHBaseVersion.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2020 Google LLC.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.cloud.bigtable.hbase;
+
+import static org.junit.Assert.assertTrue;
+
+import com.google.common.collect.ComparisonChain;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class TestBigtableHBaseVersion {
+
+  @Test
+  public void testVersionNumber() {
+    // Version must have 3 parts i.e. x.y.z and can have an optional of -SNAPSHOT suffixed.
+    Pattern versionPattern = Pattern.compile("^(\\d+)\\.(\\d+)\\.(\\d+)(-\\w+)?");
+    Matcher versionMatcher = versionPattern.matcher(BigtableHBaseVersion.getVersion());
+
+    assertTrue("incorrect version format", versionMatcher.matches());
+
+    int result =
+        ComparisonChain.start()
+            .compare(1, Integer.parseInt(versionMatcher.group(1)))
+            .compare(14, Integer.parseInt(versionMatcher.group(2)))
+            .compare(1, Integer.parseInt(versionMatcher.group(3)))
+            .result();
+
+    assertTrue("Expected BigtableHBaseVersion.getVersion() to be at least 1.14.1", result <= 0);
+  }
+}
