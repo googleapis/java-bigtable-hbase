e3fa34184c8d1ac717ed61f52f73d1378f5c2d25
Rahul Kesharwani <rahulk@qlogic.io>
2020-03-18 10:49:00 +0530

    add more unit test to improve codeCov

--- bigtable-client-core-parent/bigtable-hbase/src/test/java/com/google/cloud/bigtable/hbase/wrappers/classic/TestBigtableHBaseClassicSettings.java
+++ bigtable-client-core-parent/bigtable-hbase/src/test/java/com/google/cloud/bigtable/hbase/wrappers/classic/TestBigtableHBaseClassicSettings.java
@@ -176,4 +176,31 @@ public class TestBigtableHBaseClassicSettings {
 
     Assert.assertSame(credentials, actualCreds);
   }
+
+  @Test
+  public void testClassicSettingsGetters() throws IOException {
+    configuration.set(BigtableOptionsFactory.BIGTABLE_ADMIN_HOST_KEY, "localhost");
+    configuration.set(BigtableOptionsFactory.BIGTABLE_PORT_KEY, "8080");
+    configuration.set(BigtableOptionsFactory.BIGTABLE_EMULATOR_HOST_KEY, "localhost:8080");
+    configuration.set(BigtableOptionsFactory.APP_PROFILE_ID_KEY, "test");
+    configuration.set(BigtableOptionsFactory.BIGTABLE_BULK_MAX_ROW_KEY_COUNT, "100");
+    configuration.set(BigtableOptionsFactory.BIGTABLE_BUFFERED_MUTATOR_MAX_MEMORY_KEY, "2000");
+    configuration.set(BigtableOptionsFactory.ALLOW_NO_TIMESTAMP_RETRIES_KEY, "true");
+    configuration.set(BigtableOptionsFactory.BIGTABLE_BUFFERED_MUTATOR_ENABLE_THROTTLING, "true");
+    configuration.set(
+        BigtableOptionsFactory.BIGTABLE_BUFFERED_MUTATOR_THROTTLING_THRESHOLD_MILLIS, "500");
+    BigtableHBaseSettings settings = BigtableHBaseSettings.create(configuration);
+
+    assertEquals(TEST_HOST, settings.getDataHost());
+    assertEquals("localhost", settings.getAdminHost());
+    assertEquals(8080, settings.getPort());
+    assertEquals(100, settings.getBulkMaxRowCount());
+    assertEquals(2000, settings.getBatchingMaxRequestSize());
+    assertTrue(settings.isRetriesWithoutTimestampAllowed());
+
+    BigtableOptions options = ((BigtableHBaseClassicSettings) settings).getBigtableOptions();
+    assertEquals("test", options.getAppProfileId());
+    assertTrue(options.getBulkOptions().isEnableBulkMutationThrottling());
+    assertEquals(500, options.getBulkOptions().getBulkMutationRpcTargetMs());
+  }
 }
