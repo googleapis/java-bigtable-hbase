113b1f5ecbf56e702d5d6a38b0caee110be49993
Jeff Ching <chingor@google.com>
2020-03-18 09:17:12 -0700

    chore: use java helper for templates

--- synth.py
+++ synth.py
@@ -14,12 +14,9 @@
 
 """This script is used to synthesize generated parts of this library."""
 
-import synthtool as s
-import synthtool.gcp as gcp
+import synthtool.languages.java as java
 
-common_templates = gcp.CommonTemplates()
-templates = common_templates.java_library()
-s.copy(templates, excludes=[
+java.common_templates(excludes=[
   'README.md',
   '.github/ISSUE_TEMPLATE/bug_report.md',
   '.kokoro/presubmit/integration.cfg',
