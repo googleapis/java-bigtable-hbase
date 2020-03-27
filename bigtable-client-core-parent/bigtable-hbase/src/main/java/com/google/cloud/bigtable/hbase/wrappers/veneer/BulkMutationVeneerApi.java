d119d7a0b0c052282985a2d6379e4f3a0f7c902e
Rahul Kesharwani <rahulk@qlogic.io>
2020-03-27 20:18:33 +0530

    chore: adding Admin and BulkMutation veneer implementation
    This change adds veneer specific implementation for AdminClientWrapper and BulkMutationWrapper.

---
+++ bigtable-client-core-parent/bigtable-hbase/src/main/java/com/google/cloud/bigtable/hbase/wrappers/veneer/BulkMutationVeneerApi.java
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.cloud.bigtable.hbase.wrappers.veneer;
+
+import com.google.api.core.ApiFuture;
+import com.google.api.core.InternalApi;
+import com.google.api.gax.batching.Batcher;
+import com.google.cloud.bigtable.data.v2.models.RowMutationEntry;
+import com.google.cloud.bigtable.hbase.wrappers.BulkMutationWrapper;
+import com.google.common.base.Preconditions;
+import java.io.IOException;
+
+/** For internal use only - public for technical reasons. */
+@InternalApi("For internal usage only")
+public class BulkMutationVeneerApi implements BulkMutationWrapper {
+
+  private final Batcher<RowMutationEntry, Void> bulkMutateBatcher;
+
+  BulkMutationVeneerApi(Batcher<RowMutationEntry, Void> bulkMutateBatcher) {
+    this.bulkMutateBatcher = bulkMutateBatcher;
+  }
+
+  /** {@inheritDoc} */
+  @Override
+  public synchronized ApiFuture<Void> add(RowMutationEntry rowMutation) {
+    Preconditions.checkNotNull(rowMutation, "mutation details cannot be null");
+    return bulkMutateBatcher.add(rowMutation);
+  }
+
+  /** {@inheritDoc} */
+  @Override
+  public void sendUnsent() {
+    bulkMutateBatcher.sendOutstanding();
+  }
+
+  /** {@inheritDoc} */
+  @Override
+  public void flush() {
+    try {
+      bulkMutateBatcher.flush();
+    } catch (InterruptedException ex) {
+      throw new RuntimeException("Could not complete RPC for current Batch", ex);
+    }
+  }
+
+  @Override
+  public void close() throws IOException {
+    try {
+      bulkMutateBatcher.close();
+    } catch (InterruptedException e) {
+      throw new IOException("Could not close the bulk mutation Batcher", e);
+    }
+  }
+}
---
+++ bigtable-client-core-parent/bigtable-hbase/src/test/java/com/google/cloud/bigtable/hbase/wrappers/veneer/TestAdminClientVeneerApi.java
@@ -0,0 +1,222 @@
+/*
+ * Copyright 2020 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.cloud.bigtable.hbase.wrappers.veneer;
+
+import static org.junit.Assert.assertEquals;
+
+import com.google.bigtable.admin.v2.BigtableTableAdminGrpc;
+import com.google.bigtable.admin.v2.DeleteTableRequest;
+import com.google.bigtable.admin.v2.DropRowRangeRequest;
+import com.google.bigtable.admin.v2.GetTableRequest;
+import com.google.bigtable.admin.v2.ListTablesRequest;
+import com.google.bigtable.admin.v2.ListTablesResponse;
+import com.google.cloud.bigtable.admin.v2.BigtableTableAdminClient;
+import com.google.cloud.bigtable.admin.v2.BigtableTableAdminSettings;
+import com.google.cloud.bigtable.admin.v2.internal.NameUtil;
+import com.google.cloud.bigtable.admin.v2.models.CreateTableRequest;
+import com.google.cloud.bigtable.admin.v2.models.ModifyColumnFamiliesRequest;
+import com.google.cloud.bigtable.admin.v2.models.Table;
+import com.google.common.collect.Queues;
+import com.google.protobuf.Empty;
+import io.grpc.Server;
+import io.grpc.ServerBuilder;
+import io.grpc.stub.StreamObserver;
+import java.net.ServerSocket;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class TestAdminClientVeneerApi {
+
+  private static final String PROJECT_ID = "fake-project-id";
+  private static final String INSTANCE_ID = "fake-instance-id";
+  private static final String TABLE_ID_1 = "fake-Table-id-1";
+  private static final String TABLE_ID_2 = "fake-Table-id-2";
+
+  private FakeBigtableAdmin fakeAdminService = new FakeBigtableAdmin();
+  private Server server;
+  private BigtableTableAdminClient adminClientV2;
+  private AdminClientVeneerApi adminClientWrapper;
+
+  @Before
+  public void setUp() throws Exception {
+    final int port;
+    try (ServerSocket serverSocket = new ServerSocket(0)) {
+      port = serverSocket.getLocalPort();
+    }
+
+    server = ServerBuilder.forPort(port).addService(fakeAdminService).build();
+    server.start();
+    adminClientV2 =
+        BigtableTableAdminClient.create(
+            BigtableTableAdminSettings.newBuilderForEmulator(port)
+                .setProjectId(PROJECT_ID)
+                .setInstanceId(INSTANCE_ID)
+                .build());
+    adminClientWrapper = new AdminClientVeneerApi(adminClientV2);
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    adminClientWrapper.close();
+    adminClientV2.close();
+
+    if (server != null) {
+      server.shutdown();
+      server.awaitTermination();
+    }
+  }
+
+  @Test
+  public void testCreateTableAsync() throws Exception {
+    CreateTableRequest req = CreateTableRequest.of(TABLE_ID_1);
+    Future<Table> response = adminClientWrapper.createTableAsync(req);
+    assertEquals(TABLE_ID_1, response.get().getId());
+  }
+
+  @Test
+  public void testGetTableAsync() throws Exception {
+    Future<Table> response = adminClientWrapper.getTableAsync("test-async-table");
+    assertEquals(TABLE_ID_1, response.get().getId());
+  }
+
+  @Test
+  public void testListTablesAsync() throws Exception {
+    Future<List<String>> tableIds = adminClientWrapper.listTablesAsync();
+    assertEquals(Arrays.asList(TABLE_ID_1, TABLE_ID_2), tableIds.get());
+  }
+
+  @Test
+  public void testDeleteTableAsync() throws Exception {
+    Future<Void> deleteResponse = adminClientWrapper.deleteTableAsync("deleteAsyncTableID");
+    deleteResponse.get();
+    DeleteTableRequest deleteTableReq = fakeAdminService.popLastRequest();
+    assertEquals(
+        "deleteAsyncTableID", NameUtil.extractTableIdFromTableName(deleteTableReq.getName()));
+  }
+
+  @Test
+  public void testModifyFamilyAsync() throws Exception {
+    ModifyColumnFamiliesRequest request =
+        ModifyColumnFamiliesRequest.of(TABLE_ID_1)
+            .addFamily("first-family")
+            .addFamily("another-family");
+    Future<Table> response = adminClientWrapper.modifyFamiliesAsync(request);
+    assertEquals(TABLE_ID_1, response.get().getId());
+  }
+
+  @Test
+  public void dropRowRangeAsync() throws Exception {
+    String rowKey = "cf-dropRange-async";
+    adminClientWrapper.dropRowRangeAsync(TABLE_ID_1, rowKey).get();
+    DropRowRangeRequest rangeRequest = fakeAdminService.popLastRequest();
+    assertEquals(TABLE_ID_1, NameUtil.extractTableIdFromTableName(rangeRequest.getName()));
+    assertEquals(rowKey, rangeRequest.getRowKeyPrefix().toStringUtf8());
+  }
+
+  @Test
+  public void dropAllRowsAsync() throws Exception {
+    String tableId = "tableWithNoDataId";
+    adminClientWrapper.dropAllRowsAsync(tableId).get();
+    DropRowRangeRequest rangeRequest = fakeAdminService.popLastRequest();
+    assertEquals(tableId, NameUtil.extractTableIdFromTableName(rangeRequest.getName()));
+  }
+
+  private static class FakeBigtableAdmin extends BigtableTableAdminGrpc.BigtableTableAdminImplBase {
+
+    final BlockingQueue<Object> requests = Queues.newLinkedBlockingDeque();
+
+    @SuppressWarnings("unchecked")
+    <T> T popLastRequest() throws InterruptedException {
+      return (T) requests.poll(1, TimeUnit.SECONDS);
+    }
+
+    @Override
+    public void createTable(
+        com.google.bigtable.admin.v2.CreateTableRequest request,
+        StreamObserver<com.google.bigtable.admin.v2.Table> responseObserver) {
+      requests.add(request);
+      responseObserver.onNext(
+          com.google.bigtable.admin.v2.Table.newBuilder()
+              .setName(NameUtil.formatTableName(PROJECT_ID, INSTANCE_ID, TABLE_ID_1))
+              .build());
+      responseObserver.onCompleted();
+    }
+
+    @Override
+    public void getTable(
+        GetTableRequest request,
+        StreamObserver<com.google.bigtable.admin.v2.Table> responseObserver) {
+      requests.add(request);
+      responseObserver.onNext(
+          com.google.bigtable.admin.v2.Table.newBuilder()
+              .setName(NameUtil.formatTableName(PROJECT_ID, INSTANCE_ID, TABLE_ID_1))
+              .build());
+      responseObserver.onCompleted();
+    }
+
+    @Override
+    public void listTables(
+        ListTablesRequest request, StreamObserver<ListTablesResponse> responseObserver) {
+      requests.add(request);
+      ListTablesResponse.Builder builder = ListTablesResponse.newBuilder();
+      builder
+          .addTablesBuilder()
+          .setName(NameUtil.formatTableName(PROJECT_ID, INSTANCE_ID, TABLE_ID_1))
+          .build();
+      builder
+          .addTablesBuilder()
+          .setName(NameUtil.formatTableName(PROJECT_ID, INSTANCE_ID, TABLE_ID_2))
+          .build();
+      responseObserver.onNext(builder.build());
+      responseObserver.onCompleted();
+    }
+
+    @Override
+    public void deleteTable(DeleteTableRequest request, StreamObserver<Empty> responseObserver) {
+      requests.add(request);
+      responseObserver.onNext(Empty.newBuilder().build());
+      responseObserver.onCompleted();
+    }
+
+    @Override
+    public void modifyColumnFamilies(
+        com.google.bigtable.admin.v2.ModifyColumnFamiliesRequest request,
+        StreamObserver<com.google.bigtable.admin.v2.Table> responseObserver) {
+      requests.add(request);
+      responseObserver.onNext(
+          com.google.bigtable.admin.v2.Table.newBuilder()
+              .setName(NameUtil.formatTableName(PROJECT_ID, INSTANCE_ID, TABLE_ID_1))
+              .build());
+      responseObserver.onCompleted();
+    }
+
+    @Override
+    public void dropRowRange(DropRowRangeRequest request, StreamObserver<Empty> responseObserver) {
+      requests.add(request);
+      responseObserver.onNext(Empty.newBuilder().build());
+      responseObserver.onCompleted();
+    }
+  }
+}
