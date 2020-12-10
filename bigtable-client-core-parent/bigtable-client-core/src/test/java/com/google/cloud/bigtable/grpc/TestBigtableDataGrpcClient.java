1152271e945d3509b8d8862058effc4efce98865
Igor Bernstein <igorbernstein@google.com>
2020-12-10 14:49:26 -0500

    fix: race condition where a retry triggers a full table scan (#2742)
    * fix: fix race condition where a retry triggers a full table scan
    
    There is a race condition where a read rows stream will return all of the data, but still end in a non-ok status. This will trigger a full table scan because the row resumption logic will strip away all of seem keys & ranges, leaving the retry request empty, will cause a full table scan.
    
    This PR will detect this case and will replace the status code to be ok. This should be a very rare condition.
    
    * fix broken test
    
    * fix broken test2
    
    * format

--- bigtable-client-core-parent/bigtable-client-core/src/test/java/com/google/cloud/bigtable/grpc/TestBigtableDataGrpcClient.java
+++ bigtable-client-core-parent/bigtable-client-core/src/test/java/com/google/cloud/bigtable/grpc/TestBigtableDataGrpcClient.java
@@ -216,7 +216,7 @@ public class TestBigtableDataGrpcClient {
   @Test
   public void testScanner() throws IOException {
     ReadRowsRequest.Builder requestBuilder = ReadRowsRequest.newBuilder().setTableName(TABLE_NAME);
-    requestBuilder.getRowsBuilder().addRowKeys(ByteString.EMPTY);
+
     ResultScanner<FlatRow> scanner = defaultClient.readFlatRows(requestBuilder.build());
     ArgumentCaptor<ClientCall.Listener> listenerCaptor =
         ArgumentCaptor.forClass(ClientCall.Listener.class);
@@ -237,7 +237,7 @@ public class TestBigtableDataGrpcClient {
   @Test
   public void testScannerIdle() throws IOException {
     ReadRowsRequest.Builder requestBuilder = ReadRowsRequest.newBuilder().setTableName(TABLE_NAME);
-    requestBuilder.getRowsBuilder().addRowKeys(ByteString.EMPTY);
+
     ResultScanner<FlatRow> scanner = defaultClient.readFlatRows(requestBuilder.build());
     ArgumentCaptor<ClientCall.Listener> listenerCaptor =
         ArgumentCaptor.forClass(ClientCall.Listener.class);
