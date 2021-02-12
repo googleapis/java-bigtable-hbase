fa07a9045ed15f76fce78d3da570504c229292e2
Verma <62449205+vermas2012@users.noreply.github.com>
2021-02-12 15:37:27 -0500

    feat: Add a new pipeline to validate data imported from HBase (#2828)
    * feat: add a new pipeline to validate data imported into cloud bigtable from HBase.

---
+++ bigtable-dataflow-parent/bigtable-beam-import/src/main/java/com/google/cloud/bigtable/beam/validation/ComputeAndValidateHashFromBigtableDoFn.java
@@ -0,0 +1,217 @@
+/*
+ * Copyright 2021 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.cloud.bigtable.beam.validation;
+
+import static com.google.cloud.bigtable.beam.validation.SyncTableUtils.immutableBytesToString;
+
+import com.google.bigtable.repackaged.com.google.common.base.Preconditions;
+import com.google.bigtable.repackaged.com.google.common.collect.Lists;
+import com.google.cloud.bigtable.beam.AbstractCloudBigtableTableDoFn;
+import com.google.cloud.bigtable.beam.CloudBigtableConfiguration;
+import com.google.cloud.bigtable.beam.TemplateUtils;
+import com.google.cloud.bigtable.beam.validation.HadoopHashTableSource.RangeHash;
+import com.google.cloud.bigtable.beam.validation.SyncTableJob.SyncTableOptions;
+import com.google.common.annotations.VisibleForTesting;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+import org.apache.beam.sdk.metrics.Counter;
+import org.apache.beam.sdk.metrics.Metrics;
+import org.apache.beam.sdk.options.ValueProvider;
+import org.apache.beam.sdk.transforms.DoFn;
+import org.apache.beam.sdk.values.KV;
+import org.apache.hadoop.hbase.HConstants;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.client.ResultScanner;
+import org.apache.hadoop.hbase.client.Scan;
+import org.apache.hadoop.hbase.client.Table;
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
+import org.apache.hadoop.hbase.mapreduce.BigtableTableHashAccessor.BigtableResultHasher;
+
+/**
+ * A {@link DoFn} that takes a row range and hash from HBase and validates the hash from rows read
+ * from Cloud Bigtable.
+ */
+class ComputeAndValidateHashFromBigtableDoFn
+    extends AbstractCloudBigtableTableDoFn<KV<String, Iterable<List<RangeHash>>>, RangeHash> {
+
+  private static final long serialVersionUID = 2349094L;
+  private final ValueProvider<String> tableName;
+  private final ValueProvider<String> projectId;
+  private final ValueProvider<String> sourceHashDir;
+
+  private final TableHashWrapperFactory tableHashWrapperFactory;
+
+  // Counter for reporting matching and mismatching ranges. Names are similar to HBase sync-table
+  // job.
+  private final Counter matches = Metrics.counter("cbt-dataflow-validate", "ranges_matched");
+  private final Counter mismatches = Metrics.counter("cbt-dataflow-validate", "ranges_not_matched");
+
+  public ComputeAndValidateHashFromBigtableDoFn(SyncTableOptions options) {
+    super(TemplateUtils.BuildSyncTableConfig(options));
+    this.tableName = options.getBigtableTableId();
+    // Create a local copy of ValueProviders, PipelineOptions are not serializable.
+    projectId = options.getBigtableProject();
+    sourceHashDir = options.getHashTableOutputDir();
+    tableHashWrapperFactory = new TableHashWrapperFactory();
+  }
+
+  @VisibleForTesting
+  ComputeAndValidateHashFromBigtableDoFn(
+      CloudBigtableConfiguration config,
+      ValueProvider<String> tableName,
+      ValueProvider<String> projectId,
+      ValueProvider<String> sourceHashDir,
+      TableHashWrapperFactory factory) {
+    super(config);
+    this.tableName = tableName;
+    this.tableHashWrapperFactory = factory;
+    this.sourceHashDir = projectId;
+    this.projectId = sourceHashDir;
+  }
+
+  @ProcessElement
+  public void processElement(ProcessContext context) throws Exception {
+    List<List<RangeHash>> wrapperdRangeHashes = Lists.newArrayList(context.element().getValue());
+    // BufferedHadoopHashTableSource generates only 1 item per groupby key, key is startKey for the
+    // Sorted ranges.
+    Preconditions.checkState(
+        wrapperdRangeHashes.size() == 1, "Can not have muiple entries for a key");
+    List<RangeHash> rangeHashes = wrapperdRangeHashes.get(0);
+    Preconditions.checkState(!rangeHashes.isEmpty(), "Can not have empty ranges in DO_FN");
+
+    ImmutableBytesWritable rangeStartInclusive = rangeHashes.get(0).startInclusive;
+    ImmutableBytesWritable rangeEndExclusive =
+        rangeHashes.get(rangeHashes.size() - 1).stopExclusive;
+
+    BigtableResultHasher resultHasher = new BigtableResultHasher();
+    resultHasher.startBatch(rangeStartInclusive);
+
+    // Since all the row-ranges are sorted in HashTable's data files, 1 big scan can be used
+    // to read all the row ranges. Parallelism is achieved by splitting the HashTable's data
+    // files into smaller bundle of row-ranges in GroupBy.
+    ResultScanner scanner =
+        createBigtableScan(rangeStartInclusive.copyBytes(), rangeEndExclusive.copyBytes());
+
+    Iterator<RangeHash> rangeHashIterator = rangeHashes.iterator();
+    long numRows = 0;
+
+    RangeHash currentRangeHash = rangeHashIterator.next();
+
+    // Process each row and validate hashes
+    for (Result result : scanner) {
+      numRows++;
+      if (numRows % 10_000 == 0) {
+        // Heartbeat in logs in case a large scan gets hung.
+        DOFN_LOG.debug("Processed " + numRows + " rows ");
+      }
+
+      ImmutableBytesWritable rowKey = new ImmutableBytesWritable(result.getRow());
+
+      // Check if the rowKey belongs to current range, if not keep iterating through the
+      // rangeHashes until rowKey's range is found.
+      while (!isWithinUpperBound(currentRangeHash.stopExclusive, rowKey)) {
+        validateBatchHash(context, resultHasher, currentRangeHash);
+        // THIS SHOULD NEVER HAPPEN. Bigtable is being scanned till the last
+        // RangeHash.endKeyExclusive(), so bigtable's result should not outlast the
+        // rangeHashes.
+        Preconditions.checkState(
+            rangeHashIterator.hasNext(),
+            "Buffer reached to end while scan is still active at row : %s. "
+                + "Affected Range: [%s, %s)."
+                + immutableBytesToString(result.getRow())
+                + immutableBytesToString(rangeStartInclusive)
+                + immutableBytesToString(rangeEndExclusive));
+        currentRangeHash = rangeHashIterator.next();
+      }
+
+      // Always Hash the current row.
+      resultHasher.hashResult(result);
+    }
+
+    // Bigtable scan is finished at this point and rangeHashes may contain additional row ranges.
+    // Last range will always be unverified as the range end is exclusive and
+    // currentRow > rangeEndExclusive will never by true. Verify the last range.
+    validateBatchHash(context, resultHasher, currentRangeHash);
+
+    // If there are remaining ranges in the rangeHashes they all need to reported as mismatched as
+    // there is nothing in Cloud Bigtable for those row ranges.
+    // for (int i = bufferIndex; i < rangeHashes.size(); i++) {
+    while (rangeHashIterator.hasNext()) {
+      currentRangeHash = rangeHashIterator.next();
+      reportMismatch(context, currentRangeHash);
+    }
+
+    DOFN_LOG.debug(
+        "Finishing context by outputting {}  keys in range [{}, {}).",
+        rangeHashes.size(),
+        immutableBytesToString(rangeStartInclusive),
+        immutableBytesToString(rangeEndExclusive));
+  }
+
+  private ResultScanner createBigtableScan(byte[] startKeyInclusive, byte[] stopKeyExclusive)
+      throws IOException {
+    Table table = getConnection().getTable(TableName.valueOf(tableName.get()));
+    // Get the scan from TableHash, HashTable can be run to hash a small part of data (selected
+    // column families, timestamp range, maxVersions etc), this scan allows us to fetch the same
+    // data from Cloud Bigtable to match.
+    TableHashWrapper tableHash =
+        tableHashWrapperFactory.getTableHash(projectId.get(), sourceHashDir.get());
+    Scan scan = tableHash.getScan();
+    // Set the workitem boundaries on the scan.
+    if (startKeyInclusive.length > 0) {
+      scan.withStartRow(startKeyInclusive, true);
+    }
+    if (stopKeyExclusive.length > 0) {
+      scan.withStopRow(stopKeyExclusive, false);
+    }
+
+    return table.getScanner(scan);
+  }
+
+  /**
+   * Determines if row >= stopExclusive for a row range (start, stopExclusive). Empty stopExclusive
+   * represents a range with no upper bound.
+   */
+  private static boolean isWithinUpperBound(
+      ImmutableBytesWritable stopExclusive, ImmutableBytesWritable row) {
+    return stopExclusive.equals(HConstants.EMPTY_END_ROW) || row.compareTo(stopExclusive) < 0;
+  }
+
+  private void validateBatchHash(
+      ProcessContext context, BigtableResultHasher resultHasher, RangeHash currentRangeHash) {
+    // The batch is always started, so its safe to finish the batch. If there were no rows, we will
+    // get a hash for empty batch.
+    resultHasher.finishBatch();
+    if (!resultHasher.getBatchHash().equals(currentRangeHash.hash)) {
+      reportMismatch(context, currentRangeHash);
+    } else {
+      matches.inc();
+    }
+    // Start a new batch
+    resultHasher.startBatch(currentRangeHash.stopExclusive);
+  }
+
+  private void reportMismatch(ProcessContext context, RangeHash currentRangeHash) {
+    mismatches.inc();
+    DOFN_LOG.info(
+        "MISMATCH ON RANGE [{}, {}).",
+        immutableBytesToString(currentRangeHash.startInclusive),
+        immutableBytesToString(currentRangeHash.stopExclusive));
+    context.output(currentRangeHash);
+  }
+}
--- bigtable-dataflow-parent/bigtable-beam-import/src/test/java/com/google/cloud/bigtable/beam/hbasesnapshots/EndToEndIT.java
+++ bigtable-dataflow-parent/bigtable-beam-import/src/test/java/com/google/cloud/bigtable/beam/hbasesnapshots/EndToEndIT.java
@@ -18,37 +18,61 @@ package com.google.cloud.bigtable.beam.hbasesnapshots;
 import static com.google.common.base.Preconditions.checkNotNull;
 
 import com.google.api.services.storage.model.Objects;
-import com.google.cloud.bigtable.beam.sequencefiles.testing.BigtableTableUtils;
+import com.google.bigtable.repackaged.com.google.gson.Gson;
+import com.google.cloud.bigtable.beam.hbasesnapshots.ImportJobFromHbaseSnapshot.ImportOptions;
+import com.google.cloud.bigtable.beam.sequencefiles.HBaseResultToMutationFn;
+import com.google.cloud.bigtable.beam.validation.HadoopHashTableSource.RangeHash;
+import com.google.cloud.bigtable.beam.validation.SyncTableJob;
+import com.google.cloud.bigtable.beam.validation.SyncTableJob.SyncTableOptions;
 import com.google.cloud.bigtable.hbase.BigtableConfiguration;
 import com.google.cloud.bigtable.hbase.BigtableOptionsFactory;
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
 import java.util.UUID;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 import org.apache.beam.runners.dataflow.DataflowRunner;
 import org.apache.beam.runners.dataflow.options.DataflowPipelineOptions;
+import org.apache.beam.sdk.PipelineResult;
 import org.apache.beam.sdk.PipelineResult.State;
 import org.apache.beam.sdk.extensions.gcp.options.GcpOptions;
 import org.apache.beam.sdk.extensions.gcp.util.GcsUtil;
 import org.apache.beam.sdk.extensions.gcp.util.gcsfs.GcsPath;
+import org.apache.beam.sdk.metrics.MetricQueryResults;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.options.ValueProvider.StaticValueProvider;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
+import org.apache.hadoop.hbase.Cell;
 import org.apache.hadoop.hbase.HColumnDescriptor;
+import org.apache.hadoop.hbase.HConstants;
 import org.apache.hadoop.hbase.HTableDescriptor;
 import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.Connection;
+import org.apache.hadoop.hbase.client.Delete;
+import org.apache.hadoop.hbase.client.Get;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.client.Table;
 import org.apache.hadoop.hbase.snapshot.SnapshotTestingUtils;
 import org.junit.After;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /*
- * End to end integration test for pipeline that import HBase snapshot data into Cloud Bigtable.
+ * End to end integration test for pipeline that import HBase snapshot data into Cloud Bigtable and
+ * validates the imported data with SyncTable.
  * Prepare test data with gsutil(https://cloud.google.com/storage/docs/quickstart-gsutil):
- * gsutil -m cp -r <PATH_TO_REPO>/bigtable-dataflow-parent/bigtable-beam-import/src/test/data/ \
- *  gs://<test_bucket>/integration-test/
+ * gsutil -m cp -r <PATH_TO_REPO>/bigtable-dataflow-parent/bigtable-beam-import/src/test/integration-test \
+ *  gs://<test_bucket>/
  *
  * Setup GCP credential: https://cloud.google.com/docs/authentication
  *  Ensure your credential have access to Bigtable and Dataflow
@@ -62,7 +86,7 @@ import org.junit.Test;
  */
 public class EndToEndIT {
 
-  private final Log LOG = LogFactory.getLog(getClass());
+  private static Logger LOG = LoggerFactory.getLogger(HBaseResultToMutationFn.class);
   private static final String TEST_SNAPSHOT_NAME = "test-snapshot";
   // Location of test data hosted on Google Cloud Storage, for on-cloud dataflow tests.
   private static final String CLOUD_TEST_DATA_FOLDER = "cloud.test.data.folder";
@@ -87,6 +111,8 @@ public class EndToEndIT {
 
   // Snapshot data setup
   private String hbaseSnapshotDir;
+  private String hashDir;
+  private String syncTableOutputDir;
 
   @Before
   public void setup() throws Exception {
@@ -101,6 +127,13 @@ public class EndToEndIT {
 
     hbaseSnapshotDir = cloudTestDataFolder + "data/";
     UUID test_uuid = UUID.randomUUID();
+    hashDir = cloudTestDataFolder + "hashtable/";
+
+    syncTableOutputDir = dataflowStagingLocation;
+    if (!syncTableOutputDir.endsWith(File.separator)) {
+      syncTableOutputDir = syncTableOutputDir + File.separator;
+    }
+    syncTableOutputDir = syncTableOutputDir + "sync-table-output/" + test_uuid + "/";
 
     // Cloud Storage config
     GcpOptions gcpOptions = PipelineOptionsFactory.create().as(GcpOptions.class);
@@ -118,6 +151,12 @@ public class EndToEndIT {
     for (int i = 0; i < keys.length; i++) {
       keySplits[i] = keys[i].getBytes();
     }
+
+    // Create table in Bigtable
+    TableName tableName = TableName.valueOf(tableId);
+    HTableDescriptor descriptor = new HTableDescriptor(tableName);
+    descriptor.addFamily(new HColumnDescriptor(CF));
+    connection.getAdmin().createTable(descriptor, SnapshotTestingUtils.getSplitKeys());
   }
 
   private static String getTestProperty(String name) {
@@ -126,6 +165,19 @@ public class EndToEndIT {
 
   @After
   public void teardown() throws IOException {
+    final List<GcsPath> paths = gcsUtil.expand(GcsPath.fromUri(syncTableOutputDir + "/*"));
+
+    if (!paths.isEmpty()) {
+      final List<String> pathStrs = new ArrayList<>();
+
+      for (GcsPath path : paths) {
+        pathStrs.add(path.toString());
+      }
+      // TODO: cleanup fails when tests time out. Add a orphan cleaner in the setup()
+      // https://github.com/googleapis/java-bigtable/blob/35588d89b9b243eb691a29d3aff16b9f5a08fbb8/google-cloud-bigtable/src/test/java/com/google/cloud/bigtable/test_helpers/env/AbstractTestEnv.java#L108-L119
+      this.gcsUtil.remove(pathStrs);
+    }
+
     connection.close();
 
     // delete test table
@@ -134,18 +186,28 @@ public class EndToEndIT {
         .deleteTable(TableName.valueOf(tableId));
   }
 
-  @Test
-  public void testHBaseSnapshotImport() throws Exception {
+  private SyncTableOptions createSyncTableOptions() {
+    DataflowPipelineOptions syncTableOpts =
+        PipelineOptionsFactory.as(DataflowPipelineOptions.class);
+    syncTableOpts.setRunner(DataflowRunner.class);
+    syncTableOpts.setGcpTempLocation(dataflowStagingLocation);
+    syncTableOpts.setNumWorkers(1);
+    syncTableOpts.setProject(projectId);
 
-    // Crete table
-    TableName tableName = TableName.valueOf(tableId);
-    HTableDescriptor descriptor = new HTableDescriptor(tableName);
+    SyncTableOptions syncOpts = syncTableOpts.as(SyncTableOptions.class);
+    // Setup Bigtable params
+    syncOpts.setBigtableProject(StaticValueProvider.of(projectId));
+    syncOpts.setBigtableInstanceId(StaticValueProvider.of(instanceId));
+    syncOpts.setBigtableTableId(StaticValueProvider.of(tableId));
+    syncOpts.setBigtableAppProfileId(null);
 
-    descriptor.addFamily(new HColumnDescriptor(CF));
+    // Setup Hashes
+    syncOpts.setHashTableOutputDir(StaticValueProvider.of(hashDir));
+    syncOpts.setOutputPrefix(StaticValueProvider.of(syncTableOutputDir));
+    return syncOpts;
+  }
 
-    connection.getAdmin().createTable(descriptor, SnapshotTestingUtils.getSplitKeys());
-
-    // Start import
+  private ImportOptions createImportOptions() {
     DataflowPipelineOptions importPipelineOpts =
         PipelineOptionsFactory.as(DataflowPipelineOptions.class);
     importPipelineOpts.setRunner(DataflowRunner.class);
@@ -154,10 +216,9 @@ public class EndToEndIT {
     importPipelineOpts.setProject(projectId);
     importPipelineOpts.setRegion(region);
 
-    ImportJobFromHbaseSnapshot.ImportOptions importOpts =
-        importPipelineOpts.as(ImportJobFromHbaseSnapshot.ImportOptions.class);
+    ImportOptions importOpts = importPipelineOpts.as(ImportOptions.class);
 
-    // setup GCP and bigtable
+    // setup Bigtable options
     importOpts.setBigtableProject(StaticValueProvider.of(projectId));
     importOpts.setBigtableInstanceId(StaticValueProvider.of(instanceId));
     importOpts.setBigtableTableId(StaticValueProvider.of(tableId));
@@ -165,16 +226,78 @@ public class EndToEndIT {
     // setup HBase snapshot info
     importOpts.setHbaseSnapshotSourceDir(hbaseSnapshotDir);
     importOpts.setSnapshotName(TEST_SNAPSHOT_NAME);
+    return importOpts;
+  }
+
+  private Map<String, Long> getCountMap(PipelineResult result) {
+    MetricQueryResults metrics = result.metrics().allMetrics();
+    return StreamSupport.stream(metrics.getCounters().spliterator(), false)
+        .collect(Collectors.toMap((m) -> m.getName().getName(), (m) -> m.getAttempted()));
+  }
+
+  /**
+   * Reads the output of SyncTable job and returns a list of mismatched RangeHashes.
+   *
+   * @throws IOException
+   */
+  private List<RangeHash> readMismatchesFromOutputFiles() throws IOException {
+    Gson gson = new Gson();
+    // Find output files
+    List<GcsPath> outputFiles = gcsUtil.expand(GcsPath.fromUri(syncTableOutputDir + "*"));
+    List<RangeHash> rangeHashes = new ArrayList<>();
+
+    // Read each file line by line and create a RangeHash from it.
+    for (GcsPath outputFile : outputFiles) {
+      int size = (int) gcsUtil.fileSize(outputFile);
+      byte[] fileContents = new byte[size];
+      gcsUtil.open(outputFile).read(ByteBuffer.wrap(fileContents));
+      BufferedReader reader =
+          new BufferedReader(new InputStreamReader(new ByteArrayInputStream(fileContents)));
+      String serializedRangeHash;
+      while ((serializedRangeHash = reader.readLine()) != null) {
+        try {
+          rangeHashes.add(gson.fromJson(serializedRangeHash.trim(), RangeHash.class));
+        } catch (Exception e) {
+          LOG.error("Failed to parse JSON: [" + serializedRangeHash + "]", e);
+          throw e;
+        }
+      }
+    }
+    return rangeHashes;
+  }
+
+  // Asserts that all the rowKeys belong in mismatches.
+  // Throws AssertionException
+  private void validateRowInRangeHashes(List<byte[]> rowKeys, Iterable<RangeHash> mismatches) {
+    for (byte[] mismatchedRowKey : rowKeys) {
+      Assert.assertTrue(containsRow(mismatchedRowKey, mismatches));
+    }
+  }
+
+  // Returns true if the rowKey belongs in one of the ranges contained in rangeHashes.
+  private boolean containsRow(byte[] rowKey, Iterable<RangeHash> rangeHashes) {
+    for (RangeHash mismatchedRange : rangeHashes) {
+      // TODO: There maybe a better Range.belongs() utility function somewhere?
+      // Empty start/end key means that there is no start/end key.
+      if ((mismatchedRange.startInclusive.equals(HConstants.EMPTY_BYTE_ARRAY)
+              || mismatchedRange.startInclusive.compareTo(rowKey) <= 0)
+          && (mismatchedRange.stopExclusive.equals(HConstants.EMPTY_BYTE_ARRAY)
+              || mismatchedRange.stopExclusive.compareTo(rowKey) > 0)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  @Test
+  public void testHBaseSnapshotImport() throws Exception {
+
+    // Start import
+    ImportOptions importOpts = createImportOptions();
 
     // run pipeline
     State state = ImportJobFromHbaseSnapshot.buildPipeline(importOpts).run().waitUntilFinish();
     Assert.assertEquals(State.DONE, state);
-
-    // check data in bigtable
-    BigtableTableUtils destTable = new BigtableTableUtils(connection, tableId, CF);
-    Assert.assertEquals(
-        100 /* There are 100 rows in test snapshot*/,
-        destTable.readAllCellsFromTable().toArray().length);
 
     // check that the .restore dir used for temp files has been removed
     Objects objects =
@@ -185,6 +308,81 @@ public class EndToEndIT {
             null);
     Assert.assertNull(objects.getItems());
 
-    // TODO(vermas2012): Add more validations after this.
+    SyncTableOptions syncOpts = createSyncTableOptions();
+
+    PipelineResult result = SyncTableJob.buildPipeline(syncOpts).run();
+    state = result.waitUntilFinish();
+    Assert.assertEquals(State.DONE, state);
+
+    // Read the output files and validate that there are no mismatches.
+    Assert.assertEquals(0, readMismatchesFromOutputFiles().size());
+
+    // Validate the counters.
+    Map<String, Long> counters = getCountMap(result);
+    Assert.assertEquals(counters.get("ranges_matched"), (Long) 101L);
+    Assert.assertNull(counters.get("ranges_not_matched"));
+  }
+
+  /**
+   * Introduces multiple corruptions in imported table and validates that sync-table can detect
+   * them.
+   */
+  @Test
+  public void testHBaseSnapshotImportWithCorruptions() throws Exception {
+    // Import snapshot
+    ImportOptions importOpts = createImportOptions();
+    State state = ImportJobFromHbaseSnapshot.buildPipeline(importOpts).run().waitUntilFinish();
+    Assert.assertEquals(State.DONE, state);
+
+    // Rows where corruptions will be added.
+    byte[] mismatchRowAtStart = "000".getBytes();
+    byte[] mismatchRowInMiddle = "24".getBytes();
+    byte[] mismatchRowDeleted = "64".getBytes();
+    byte[] mismatchRowAtTheEnd = "999".getBytes();
+
+    // Introduce corruptions to the data in Bigtable. Delete data from Bigtable to simulate Bigtable
+    // missing data. Add data to Bigtable to simulate extra data in Bigtable. It is easier to update
+    // Bigtable than change the snapshots.
+    Table table = connection.getTable(TableName.valueOf(tableId));
+    Cell cellInMiddle = table.get(new Get(mismatchRowInMiddle)).rawCells()[0];
+    List<Put> puts =
+        Arrays.asList(
+            // Add a row at the start
+            new Put(mismatchRowAtStart)
+                .addColumn(CF.getBytes(), "random_col".getBytes(), 1L, "value000".getBytes())
+                .addColumn(CF.getBytes(), "random_col".getBytes(), 2L, "value001".getBytes()),
+            // change a cell in middle
+            new Put(cellInMiddle.getRowArray())
+                .addColumn(
+                    cellInMiddle.getFamilyArray(),
+                    cellInMiddle.getQualifierArray(),
+                    cellInMiddle.getTimestamp(),
+                    "corrupted_val".getBytes()),
+            // add a new row in the end
+            new Put(mismatchRowAtTheEnd)
+                .addColumn(CF.getBytes(), "random_col".getBytes(), 100L, "value999".getBytes()));
+
+    table.put(puts);
+    // Delete a random row in the middle. We should see 4 ranges mismatch as table is split on
+    // 1,2...9. All the updates are happening on a different split.
+    table.delete(new Delete(mismatchRowDeleted));
+
+    // Run SyncTable job and expect 4 mismatches.
+    SyncTableOptions syncOpts = createSyncTableOptions();
+    PipelineResult result = SyncTableJob.buildPipeline(syncOpts).run();
+    state = result.waitUntilFinish();
+    Assert.assertEquals(State.DONE, state);
+
+    List<RangeHash> syncTableOutputMismatches = readMismatchesFromOutputFiles();
+    Assert.assertEquals(4, syncTableOutputMismatches.size());
+    validateRowInRangeHashes(
+        Arrays.asList(
+            mismatchRowAtStart, mismatchRowAtTheEnd, mismatchRowDeleted, mismatchRowInMiddle),
+        syncTableOutputMismatches);
+
+    // Assert that the output collection is the right one.
+    Map<String, Long> counters = getCountMap(result);
+    Assert.assertEquals(counters.get("ranges_matched"), (Long) 97L);
+    Assert.assertEquals(counters.get("ranges_not_matched"), (Long) 4L);
   }
 }
