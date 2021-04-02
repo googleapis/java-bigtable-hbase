f30b23b55a4d75785bbdef572825f0d2e00c8e21
Igor Bernstein <igorbernstein@google.com>
2021-04-02 11:43:31 -0400

    chore: port cached channel pool to veneer backend
    This is an existing feature in bigtable-client-core that needed to be re-implemented for the transition to java-bigtable

---
+++ bigtable-client-core-parent/bigtable-hbase/src/main/java/com/google/cloud/bigtable/hbase/wrappers/veneer/SharedDataClentWrapper.java
@@ -0,0 +1,106 @@
+/*
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.cloud.bigtable.hbase.wrappers.veneer;
+
+import com.google.api.core.ApiFuture;
+import com.google.cloud.bigtable.data.v2.models.ConditionalRowMutation;
+import com.google.cloud.bigtable.data.v2.models.Filters.Filter;
+import com.google.cloud.bigtable.data.v2.models.KeyOffset;
+import com.google.cloud.bigtable.data.v2.models.Query;
+import com.google.cloud.bigtable.data.v2.models.ReadModifyWriteRow;
+import com.google.cloud.bigtable.data.v2.models.RowMutation;
+import com.google.cloud.bigtable.hbase.wrappers.BulkMutationWrapper;
+import com.google.cloud.bigtable.hbase.wrappers.BulkReadWrapper;
+import com.google.cloud.bigtable.hbase.wrappers.DataClientWrapper;
+import com.google.cloud.bigtable.hbase.wrappers.veneer.SharedDataClientWrapperFactory.Key;
+import com.google.protobuf.ByteString;
+import io.grpc.stub.StreamObserver;
+import java.io.IOException;
+import java.util.List;
+import javax.annotation.Nullable;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.client.ResultScanner;
+
+/** Simple wrapper around a {@link DataClientWrapper} that will release resources on close. */
+class SharedDataClentWrapper implements DataClientWrapper {
+  private final SharedDataClientWrapperFactory owner;
+  private final SharedDataClientWrapperFactory.Key key;
+  private final DataClientWrapper delegate;
+
+  public SharedDataClentWrapper(
+      SharedDataClientWrapperFactory owner, Key key, DataClientWrapper delegate) {
+    this.owner = owner;
+    this.key = key;
+    this.delegate = delegate;
+  }
+
+  @Override
+  public BulkMutationWrapper createBulkMutation(String tableId) {
+    return delegate.createBulkMutation(tableId);
+  }
+
+  @Override
+  public BulkReadWrapper createBulkRead(String tableId) {
+    return delegate.createBulkRead(tableId);
+  }
+
+  @Override
+  public ApiFuture<Void> mutateRowAsync(RowMutation rowMutation) {
+    return delegate.mutateRowAsync(rowMutation);
+  }
+
+  @Override
+  public ApiFuture<Result> readModifyWriteRowAsync(ReadModifyWriteRow readModifyWriteRow) {
+    return delegate.readModifyWriteRowAsync(readModifyWriteRow);
+  }
+
+  @Override
+  public ApiFuture<Boolean> checkAndMutateRowAsync(ConditionalRowMutation conditionalRowMutation) {
+    return delegate.checkAndMutateRowAsync(conditionalRowMutation);
+  }
+
+  @Override
+  public ApiFuture<List<KeyOffset>> sampleRowKeysAsync(String tableId) {
+    return delegate.sampleRowKeysAsync(tableId);
+  }
+
+  @Override
+  public ApiFuture<Result> readRowAsync(
+      String tableId, ByteString rowKey, @Nullable Filter filter) {
+    return delegate.readRowAsync(tableId, rowKey, filter);
+  }
+
+  @Override
+  public ResultScanner readRows(Query request) {
+    return delegate.readRows(request);
+  }
+
+  @Override
+  public ApiFuture<List<Result>> readRowsAsync(Query request) {
+    return delegate.readRowsAsync(request);
+  }
+
+  @Override
+  public void readRowsAsync(Query request, StreamObserver<Result> observer) {
+    delegate.readRowsAsync(request, observer);
+  }
+
+  @Override
+  public void close() throws IOException {
+    delegate.close();
+    owner.release(key);
+  }
+}
---
+++ bigtable-client-core-parent/bigtable-hbase/src/main/java/com/google/cloud/bigtable/hbase/wrappers/veneer/SharedDataClientWrapperFactory.java
@@ -0,0 +1,147 @@
+/*
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.cloud.bigtable.hbase.wrappers.veneer;
+
+import com.google.api.gax.core.BackgroundResource;
+import com.google.api.gax.core.CredentialsProvider;
+import com.google.api.gax.core.FixedCredentialsProvider;
+import com.google.api.gax.core.FixedExecutorProvider;
+import com.google.api.gax.rpc.ClientContext;
+import com.google.api.gax.rpc.FixedHeaderProvider;
+import com.google.api.gax.rpc.FixedTransportChannelProvider;
+import com.google.api.gax.rpc.FixedWatchdogProvider;
+import com.google.cloud.bigtable.data.v2.BigtableDataClient;
+import com.google.cloud.bigtable.data.v2.BigtableDataSettings;
+import com.google.cloud.bigtable.data.v2.BigtableDataSettings.Builder;
+import com.google.cloud.bigtable.data.v2.stub.EnhancedBigtableStubSettings;
+import com.google.cloud.bigtable.hbase.wrappers.DataClientWrapper;
+import com.google.common.base.Objects;
+import com.google.common.base.Preconditions;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Manages shared resources between multiple clients.
+ *
+ * <p>This class is meant to support channel pool caching feature.
+ */
+class SharedDataClientWrapperFactory {
+  private final Map<Key, ClientContext> cachedContexts = new HashMap<>();
+  private final Map<Key, Integer> refCounts = new HashMap<>();
+
+  synchronized DataClientWrapper createDataClient(BigtableHBaseVeneerSettings settings)
+      throws IOException {
+    Preconditions.checkArgument(
+        !settings.getDataSettings().isRefreshingChannel(),
+        "Channel refreshing is not compatible with cached channel pools");
+
+    Key key = Key.createFromSettings(settings.getDataSettings());
+
+    // Get or create ClientContext that will contained the shared resources
+    ClientContext sharedCtx = cachedContexts.get(key);
+    if (sharedCtx == null) {
+      sharedCtx = ClientContext.create(settings.getDataSettings().getStubSettings());
+      cachedContexts.put(key, sharedCtx);
+      refCounts.put(key, 0);
+    }
+    // Increment the count
+    refCounts.put(key, refCounts.get(key) + 1);
+
+    try {
+      // Patch settings to use shared resources
+      Builder builder = settings.getDataSettings().toBuilder();
+      builder
+          .stubSettings()
+          .setTransportChannelProvider(
+              FixedTransportChannelProvider.create(sharedCtx.getTransportChannel()))
+          .setCredentialsProvider(FixedCredentialsProvider.create(sharedCtx.getCredentials()))
+          .setExecutorProvider(FixedExecutorProvider.create(sharedCtx.getExecutor()))
+          .setStreamWatchdogProvider(FixedWatchdogProvider.create(sharedCtx.getStreamWatchdog()))
+          .setHeaderProvider(FixedHeaderProvider.create(sharedCtx.getHeaders()))
+          .setClock(sharedCtx.getClock());
+
+      // Create a reference counted client wrapper
+      return new SharedDataClentWrapper(
+          this, key, new DataClientVeneerApi(BigtableDataClient.create(builder.build())));
+    } catch (IOException | RuntimeException e) {
+      release(key);
+      throw e;
+    }
+  }
+
+  synchronized void release(Key key) {
+    int refCount = refCounts.get(key);
+    if (--refCount > 0) {
+      refCounts.put(key, refCount);
+      return;
+    }
+
+    refCounts.remove(key);
+    ClientContext clientContext = cachedContexts.remove(key);
+    for (BackgroundResource resource : clientContext.getBackgroundResources()) {
+      resource.shutdown();
+    }
+  }
+
+  /**
+   * Identity for a shared {@link ClientContext}.
+   *
+   * <p>This value class contains primary identifying information for a ClientContext. It can be
+   * extracted from {@link BigtableDataSettings} and be used to check if a ClientContext would be
+   * compatible with a ClientContext required by {@link BigtableDataSettings}.
+   */
+  static final class Key {
+    private final String endpoint;
+    private final Map<String, String> headers;
+    private final CredentialsProvider credentialsProvider;
+
+    static Key createFromSettings(BigtableDataSettings settings) {
+      EnhancedBigtableStubSettings stubSettings = settings.getStubSettings();
+
+      return new Key(
+          stubSettings.getEndpoint(),
+          stubSettings.getHeaderProvider().getHeaders(),
+          stubSettings.getCredentialsProvider());
+    }
+
+    private Key(
+        String endpoint, Map<String, String> headers, CredentialsProvider credentialsProvider) {
+      this.endpoint = endpoint;
+      this.headers = headers;
+      this.credentialsProvider = credentialsProvider;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+      if (this == o) {
+        return true;
+      }
+      if (o == null || getClass() != o.getClass()) {
+        return false;
+      }
+      Key key = (Key) o;
+      return Objects.equal(endpoint, key.endpoint)
+          && Objects.equal(headers, key.headers)
+          && Objects.equal(credentialsProvider, key.credentialsProvider);
+    }
+
+    @Override
+    public int hashCode() {
+      return Objects.hashCode(endpoint, headers, credentialsProvider);
+    }
+  }
+}
