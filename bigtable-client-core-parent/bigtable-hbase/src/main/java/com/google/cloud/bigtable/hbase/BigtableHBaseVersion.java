8e741f1a13d0267e550a6ea25286a6f0b50133a9
Igor Bernstein <igorbernstein@google.com>
2021-07-14 13:53:28 -0400

    chore: fix magic version comments

--- bigtable-client-core-parent/bigtable-hbase/src/main/java/com/google/cloud/bigtable/hbase/BigtableHBaseVersion.java
+++ bigtable-client-core-parent/bigtable-hbase/src/main/java/com/google/cloud/bigtable/hbase/BigtableHBaseVersion.java
@@ -33,7 +33,7 @@ public class BigtableHBaseVersion {
 
   private static final AtomicBoolean wasInitialized = new AtomicBoolean(false);
 
-  // {x-version-update-start:bigtable-client-parent}
+  // {x-version-update-start:bigtable-client-parent:current}
   public static final String VERSION = "1.22.1-SNAPSHOT";
   // {x-version-update-end}
 
