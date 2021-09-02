50bc12d0f3abda6daf018548da818b6cc2cc0d16
Mateusz Walkiewicz <mwalkiewicz@unoperate.com>
2021-09-02 17:06:17 +0200

    MirroringConnection: use new config options (#17)

--- bigtable-hbase-mirroring-client-1.x-parent/bigtable-hbase-mirroring-client-1.x/src/main/java/com/google/cloud/bigtable/mirroring/hbase1_x/MirroringConnection.java
+++ bigtable-hbase-mirroring-client-1.x-parent/bigtable-hbase-mirroring-client-1.x/src/main/java/com/google/cloud/bigtable/mirroring/hbase1_x/MirroringConnection.java
@@ -15,8 +15,19 @@
  */
 package com.google.cloud.bigtable.mirroring.hbase1_x;
 
+import com.google.cloud.bigtable.mirroring.hbase1_x.utils.ListenableReferenceCounter;
+import com.google.cloud.bigtable.mirroring.hbase1_x.utils.flowcontrol.FlowControlStrategy;
+import com.google.cloud.bigtable.mirroring.hbase1_x.utils.flowcontrol.FlowController;
+import com.google.cloud.bigtable.mirroring.hbase1_x.verification.MismatchDetector;
 import java.io.IOException;
+import java.io.InterruptedIOException;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.Admin;
@@ -29,9 +40,14 @@ import org.apache.hadoop.hbase.client.Table;
 import org.apache.hadoop.hbase.security.User;
 
 public class MirroringConnection implements Connection {
+
+  private final FlowController flowController;
   private MirroringConfiguration configuration;
   private Connection primaryConnection;
   private Connection secondaryConnection;
+  private final ExecutorService executorService;
+  private final MismatchDetector mismatchDetector;
+  private final ListenableReferenceCounter referenceCounter;
 
   /**
    * The constructor called from {@link
@@ -49,6 +65,44 @@ public class MirroringConnection implements Connection {
         ConnectionFactory.createConnection(this.configuration.primaryConfiguration, pool, user);
     this.secondaryConnection =
         ConnectionFactory.createConnection(this.configuration.secondaryConfiguration, pool, user);
+
+    if (pool == null) {
+      this.executorService = Executors.newCachedThreadPool();
+    } else {
+      this.executorService = pool;
+    }
+
+    referenceCounter = new ListenableReferenceCounter();
+    this.flowController =
+        new FlowController(
+            this.<FlowControlStrategy>construct(
+                this.configuration.mirroringOptions.flowControllerStrategyClass,
+                this.configuration.mirroringOptions));
+    this.mismatchDetector = construct(this.configuration.mirroringOptions.mismatchDetectorClass);
+  }
+
+  private <T> T construct(String className, Object... params) {
+    List<Class<?>> constructorArgs = new ArrayList<>();
+    for (Object param : params) {
+      constructorArgs.add(param.getClass());
+    }
+    Constructor<T> constructor =
+        getConstructor(className, constructorArgs.toArray(new Class<?>[0]));
+    try {
+      return constructor.newInstance(params);
+    } catch (InstantiationException | IllegalAccessException | InvocationTargetException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  private <T> Constructor<T> getConstructor(String className, Class<?>... parameterTypes) {
+    try {
+      @SuppressWarnings("unchecked")
+      Class<T> c = (Class<T>) Class.forName(className);
+      return c.getDeclaredConstructor(parameterTypes);
+    } catch (ClassNotFoundException | ClassCastException | NoSuchMethodException e) {
+      throw new RuntimeException(e);
+    }
   }
 
   @Override
@@ -58,12 +112,28 @@ public class MirroringConnection implements Connection {
 
   @Override
   public Table getTable(TableName tableName) throws IOException {
-    throw new UnsupportedOperationException();
+    MirroringTable table =
+        new MirroringTable(
+            this.primaryConnection.getTable(tableName),
+            this.secondaryConnection.getTable(tableName),
+            this.executorService,
+            this.mismatchDetector,
+            this.flowController);
+    this.referenceCounter.holdReferenceUntilClosing(table);
+    return table;
   }
 
   @Override
   public Table getTable(TableName tableName, ExecutorService executorService) throws IOException {
-    throw new UnsupportedOperationException();
+    MirroringTable table =
+        new MirroringTable(
+            this.primaryConnection.getTable(tableName, executorService),
+            this.secondaryConnection.getTable(tableName, executorService),
+            executorService,
+            this.mismatchDetector,
+            this.flowController);
+    this.referenceCounter.holdReferenceUntilClosing(table);
+    return table;
   }
 
   @Override
@@ -89,7 +159,16 @@ public class MirroringConnection implements Connection {
 
   @Override
   public void close() throws IOException {
-    throw new UnsupportedOperationException();
+    this.referenceCounter.decrementReferenceCount();
+    try {
+      this.referenceCounter.getOnLastReferenceClosed().get();
+    } catch (InterruptedException e) {
+      IOException wrapperException = new InterruptedIOException();
+      wrapperException.initCause(e);
+      throw wrapperException;
+    } catch (ExecutionException e) {
+      throw new RuntimeException(e);
+    }
   }
 
   @Override
