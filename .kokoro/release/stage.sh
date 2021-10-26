78c5e576953d38a3b6dad05b8e3e4a0987708719
kolea2 <45548808+kolea2@users.noreply.github.com>
2021-10-26 12:52:35 -0400

    build: only build bigtable-hbase-beam and its dependencies for releases (#2994) (#3312)

--- .kokoro/release/stage.sh
+++ .kokoro/release/stage.sh
@@ -28,9 +28,11 @@ setup_environment_secrets
 create_settings_xml_file "settings.xml"
 
 # attempt to stage 3 times with exponential backoff (starting with 10 seconds)
+# only build bigtable-hbase-beam and its dependents for sp branch
 retry_with_backoff 3 10 \
   mvn clean deploy -B \
     --settings ${MAVEN_SETTINGS_FILE} \
+    -am -pl  bigtable-dataflow-parent/bigtable-hbase-beam \
     -DskipTests=true \
     -DperformRelease=true \
     -Dgpg.executable=gpg \
