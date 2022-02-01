1af283b5d2a53a60e4ffeaa4de74307d2039a5e9
Sushan Bhattarai <sushantsusan@google.com>
2022-02-01 14:00:58 -0500

    add test

---
+++ bigtable-hbase-2.x-replication/src/main/java/com/google/cloud/bigtable/hbase2_x/replication/HbaseToCloudBigtableReplicationEndpoint.java
@@ -0,0 +1,63 @@
+package com.google.cloud.bigtable.hbase2_x.replication;
+
+import com.google.cloud.bigtable.hbase.replication.CloudBigtableReplicator;
+import com.google.cloud.bigtable.hbase.replication.adapters.BigtableWALEntry;
+import org.apache.hadoop.hbase.replication.BaseReplicationEndpoint;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import static java.util.stream.Collectors.groupingBy;
+
+
+public class HbaseToCloudBigtableReplicationEndpoint extends BaseReplicationEndpoint {
+    private static final Logger LOG =
+        LoggerFactory.getLogger(HbaseToCloudBigtableReplicationEndpoint.class);
+
+    private final CloudBigtableReplicator cloudBigtableReplicator;
+    public HbaseToCloudBigtableReplicationEndpoint() {
+        super();
+        cloudBigtableReplicator = new CloudBigtableReplicator();
+    }
+
+    @Override
+    public UUID getPeerUUID() {
+        return cloudBigtableReplicator.getPeerUUID(); }
+
+    @Override
+    public boolean replicate(ReplicateContext replicateContext) {
+        Map<String, List<BigtableWALEntry>> walEntriesByTable =
+            replicateContext.getEntries().stream()
+            .map(entry -> new BigtableWALEntryImpl(entry).getBigtableWALEntry())
+            .collect(groupingBy(e -> e.getTableName()));
+        return cloudBigtableReplicator.replicate(walEntriesByTable);
+    }
+
+    @Override
+    public void start() {
+        startAsync();
+
+    }
+
+    @Override
+    public void stop() {
+        stopAsync();
+
+    }
+
+    @Override
+    protected void doStart() {
+        LOG.error(
+            "Starting replication to CBT. ", new RuntimeException("Dummy exception for stacktrace."));
+        cloudBigtableReplicator.start(ctx.getConfiguration(), ctx.getMetrics());
+        notifyStarted();
+
+    }
+
+    @Override
+    protected void doStop() {
+        LOG.error("Stopping replication to CBT for this EndPoint. ",
+            new RuntimeException("Dummy exception for stacktrace"));
+        cloudBigtableReplicator.stop();
+        notifyStopped();
+    }
+}
