39164a901f4217344afff1994839bf1764c25f62
Sushan Bhattarai <sushantsusan@google.com>
2022-02-01 14:05:41 -0500

    fix a import error

--- bigtable-hbase-2.x-replication/src/main/java/com/google/cloud/bigtable/hbase2_x/replication/HbaseToCloudBigtableReplicationEndpoint.java
+++ bigtable-hbase-2.x-replication/src/main/java/com/google/cloud/bigtable/hbase2_x/replication/HbaseToCloudBigtableReplicationEndpoint.java
@@ -6,6 +6,10 @@ import org.apache.hadoop.hbase.replication.BaseReplicationEndpoint;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
 import static java.util.stream.Collectors.groupingBy;
 
 
