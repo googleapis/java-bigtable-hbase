9fa5e04eee93b78032aa3c9e7ffd9da18a9861b0
Sushan Bhattarai <sushantsusan@google.com>
2022-03-15 17:40:43 -0400

    fix

--- hbase-migration-tools/bigtable-hbase-replication/bigtable-hbase-2.x-replication/src/test/java/com/google/cloud/bigtable/hbase2_x/replication/HbaseToCloudBigtableReplicationEndpointTest.java
+++ hbase-migration-tools/bigtable-hbase-replication/bigtable-hbase-2.x-replication/src/test/java/com/google/cloud/bigtable/hbase2_x/replication/HbaseToCloudBigtableReplicationEndpointTest.java
@@ -427,31 +427,6 @@ public class HbaseToCloudBigtableReplicationEndpointTest {
   }
 
   @Test
-  public void testTimestampOverflow() throws IOException, InterruptedException {
-    Put put = new Put(TestUtils.ROW_KEY);
-    byte[] val = Bytes.toBytes(1);
-    put.addColumn(TestUtils.CF1, TestUtils.COL_QUALIFIER, Long.MAX_VALUE - 1, val);
-    hbaseTable.put(put);
-
-    TestUtils.waitForReplication(
-        () -> {
-          // 1put
-          return TestReplicationEndpoint.replicatedEntries.get() >= 1;
-        });
-
-    List<Cell> expectedCells =
-        hbaseTable.get(new Get(TestUtils.ROW_KEY).setMaxVersions()).listCells();
-    List<Cell> actualCells = cbtTable.get(new Get(TestUtils.ROW_KEY).setMaxVersions()).listCells();
-
-    for (int i = 0; i < expectedCells.size(); i++) {
-      Assert.assertNotEquals(
-          "Timestamp mismatch for row " + TestUtils.ROW_KEY,
-          expectedCells.get(i).getTimestamp(),
-          actualCells.get(i).getTimestamp());
-    }
-  }
-
-  @Test
   public void testHBaseCBTTimestampTruncation() throws IOException, InterruptedException {
     Put put = new Put(TestUtils.ROW_KEY);
     byte[] val = Bytes.toBytes(1);
