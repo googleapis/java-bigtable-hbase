407f0a03ad5f831acd3d4e5346c97310b5d10ed9
Igor Bernstein <igorbernstein@google.com>
2022-07-28 17:24:41 -0400

    try to fix integration tests

--- hbase-migration-tools/mirroring-client/run_mirroring_integration_tests.sh
+++ hbase-migration-tools/mirroring-client/run_mirroring_integration_tests.sh
@@ -7,16 +7,16 @@ scriptDir=$(realpath $(dirname "${BASH_SOURCE[0]}"))
 cd ${scriptDir}/../..
 
 targets=(
-  hbase-migration-tools/mirroring-client/bigtable-hbase-mirroring-client-core-parent/protobuf-java-format-shaded
-  hbase-migration-tools/mirroring-client/bigtable-hbase-mirroring-client-core-parent/bigtable-hbase-mirroring-client-core
-  hbase-migration-tools/mirroring-client/bigtable-hbase-mirroring-client-1.x-parent/bigtable-hbase-mirroring-client-1.x
   hbase-migration-tools/mirroring-client/bigtable-hbase-mirroring-client-1.x-parent/bigtable-hbase-mirroring-client-1.x-integration-tests
-  hbase-migration-tools/mirroring-client/bigtable-hbase-mirroring-client-2.x-parent/bigtable-hbase-mirroring-client-2.x
   hbase-migration-tools/mirroring-client/bigtable-hbase-mirroring-client-2.x-parent/bigtable-hbase-mirroring-client-2.x-integration-tests
   hbase-migration-tools/mirroring-client/bigtable-hbase-mirroring-client-2.x-parent/bigtable-hbase-mirroring-client-1.x-2.x-integration-tests
 )
 
-cmd="mvn clean compile verify -pl $( IFS=, ; echo "${targets[*]}" ) -Penable-integration-tests,"
+# install all of the dependencies into the local repository
+mvn clean install -DskipTests -am -pl $( IFS=, ; echo "${targets[*]}" )
+
+# Then run integration tests on the leaves
+cmd="mvn verify -pl $( IFS=, ; echo "${targets[*]}" ) -Penable-integration-tests,"
 
 hbaseToBigtableTargets=HBaseToBigtableLocalIntegrationTests,HBase12ToBigtableLocalIntegrationTests,HBase2ToBigtableLocalIntegrationTests
 bigtableToHbaseTargets=BigtableToHBaseLocalIntegrationTests,BigtableToHBase12LocalIntegrationTests,BigtableToHBase2LocalIntegrationTests
