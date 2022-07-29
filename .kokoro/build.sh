fd7c732dd41e58bbf836551ed3807427628d4cef
Igor Bernstein <igorbernstein@google.com>
2022-07-29 15:31:07 -0400

    increase memory cap for migration tests

--- .kokoro/build.sh
+++ .kokoro/build.sh
@@ -56,6 +56,7 @@ integration)
     ;;
 integration-migration)
    apt update && apt -y install google-cloud-sdk-bigtable-emulator
+   export MAVEN_OPTS="-Xmx2048m ${MAVEN_OPTS}"
   ./hbase-migration-tools/mirroring-client/run_mirroring_integration_tests.sh
   RETURN_CODE=$?
   ;;
