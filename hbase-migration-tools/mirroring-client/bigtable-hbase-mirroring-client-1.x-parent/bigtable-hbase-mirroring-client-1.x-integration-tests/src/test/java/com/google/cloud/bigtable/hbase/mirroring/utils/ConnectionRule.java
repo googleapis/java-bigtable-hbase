ad74264f00b67fb21a8a985c3236352cc593aa48
Igor Bernstein <igorbernstein@google.com>
2022-08-01 11:05:18 -0400

    revert debug code

--- hbase-migration-tools/mirroring-client/bigtable-hbase-mirroring-client-1.x-parent/bigtable-hbase-mirroring-client-1.x-integration-tests/src/test/java/com/google/cloud/bigtable/hbase/mirroring/utils/ConnectionRule.java
+++ hbase-migration-tools/mirroring-client/bigtable-hbase-mirroring-client-1.x-parent/bigtable-hbase-mirroring-client-1.x-integration-tests/src/test/java/com/google/cloud/bigtable/hbase/mirroring/utils/ConnectionRule.java
@@ -38,27 +38,6 @@ public class ConnectionRule extends ExternalResource {
 
   @Override
   public void before() throws Throwable {
-    {
-      Runtime runtime = Runtime.getRuntime();
-
-      java.text.NumberFormat format = java.text.NumberFormat.getInstance();
-
-      StringBuilder sb = new StringBuilder();
-      long maxMemory = runtime.maxMemory();
-      long allocatedMemory = runtime.totalMemory();
-      long freeMemory = runtime.freeMemory();
-
-      sb.append("free memory: " + format.format(freeMemory / 1024) + "\n");
-      sb.append("allocated memory: " + format.format(allocatedMemory / 1024) + "\n");
-      sb.append("max memory: " + format.format(maxMemory / 1024) + "\n");
-      sb.append(
-          "total free memory: "
-              + format.format((freeMemory + (maxMemory - allocatedMemory)) / 1024)
-              + "\n");
-
-      System.out.println(sb);
-    }
-
     if (baseMiniCluster != null) {
       baseMiniCluster.start();
     }
