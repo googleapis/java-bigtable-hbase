da23e27873dbb63f3e937e554fa8e3f678d4b328
Igor Bernstein <igorbernstein@google.com>
2023-03-21 15:48:23 -0400

    chore: make bigtable-hbase a drop in migration tool part 1 - hbase 1 libraries (#4014)
    * chore: make bigtable-hbase a drop in migration tool part 1 - hbase 1 libraries
    
    This should make bigtable-hbase's role a bit clearer: to be a drop in shim to help migrate from hbase to bigtable for existing applications instead of being a standalone client. To this end, all library jars will weaken their dependency on hbase-client to be `provided` by the enduser. The primary interaction will be via the -shaded jars tgat endusers will be able to drop into their $HBASE_CLASSPATH and then connect to bigtable using a tweaked hbase-site.xml. This interaction will emphasis minimal code changes.
    
    This PR focuses on HBase 1 libraries:
    - all dependencies on hbase are changed to provided scope
    - `-hadoop` modules are removed since they are identical to `-shaded` modules
    - maven plugin to ensure that we don't clobbered provided dependencies
    - shading configs simplified due to provided scope
    - removal of slf4j version enforcement since those deps are fully provided by hbase now
    
    Change-Id: I7946c06284c81caa0a28fc7ae4773d85fab4381a
    
    * tweaks
    
    Change-Id: I085b4e1371a008c23a59a5f69673eb0b08ef7471
    
    * format
    
    Change-Id: I32d3cda81941927cc14d3609cd73db30a2ef5682

---
+++ bigtable-test/bigtable-build-helper/src/main/java/com/google/cloud/bigtable/test/plugins/VerifyProvidedDeps.java
@@ -0,0 +1,178 @@
+/*
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.cloud.bigtable.test.plugins;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.Stack;
+import java.util.TreeSet;
+import org.apache.maven.artifact.Artifact;
+import org.apache.maven.artifact.resolver.filter.ScopeArtifactFilter;
+import org.apache.maven.execution.MavenSession;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugins.annotations.Component;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.plugins.annotations.ResolutionScope;
+import org.apache.maven.project.DefaultProjectBuildingRequest;
+import org.apache.maven.project.MavenProject;
+import org.apache.maven.project.ProjectBuildingRequest;
+import org.apache.maven.shared.dependency.graph.DependencyCollectorBuilder;
+import org.apache.maven.shared.dependency.graph.DependencyCollectorBuilderException;
+import org.apache.maven.shared.dependency.graph.DependencyNode;
+import org.apache.maven.shared.dependency.graph.traversal.DependencyNodeVisitor;
+import org.eclipse.aether.util.artifact.JavaScopes;
+
+@Mojo(
+    name = "verify-provided-deps",
+    defaultPhase = LifecyclePhase.VALIDATE,
+    requiresDependencyCollection = ResolutionScope.TEST,
+    threadSafe = true)
+public class VerifyProvidedDeps extends AbstractMojo {
+  @Parameter(defaultValue = "${project}", readonly = true, required = true)
+  private MavenProject project;
+
+  @Parameter(defaultValue = "${session}", readonly = true, required = true)
+  private MavenSession session;
+
+  @Component(hint = "default")
+  private DependencyCollectorBuilder dependencyCollectorBuilder;
+
+  @Override
+  public void execute() throws MojoExecutionException {
+    ProjectBuildingRequest buildingRequest =
+        new DefaultProjectBuildingRequest(session.getProjectBuildingRequest());
+    buildingRequest.setProject(project);
+
+    final DependencyNode rootNode;
+
+    try {
+      rootNode =
+          dependencyCollectorBuilder.collectDependencyGraph(
+              buildingRequest, new ScopeArtifactFilter(Artifact.SCOPE_COMPILE_PLUS_RUNTIME));
+    } catch (DependencyCollectorBuilderException exception) {
+      throw new MojoExecutionException("Cannot build project dependency graph", exception);
+    }
+
+    ProvidedDepsCollector collector = new ProvidedDepsCollector();
+    rootNode.accept(collector);
+
+    ErrorCollector conflictCollector = new ErrorCollector(collector.getResults());
+    rootNode.accept(conflictCollector);
+
+    for (String error : new TreeSet<>(conflictCollector.getErrors())) {
+      getLog().error(error);
+    }
+    if (!conflictCollector.getErrors().isEmpty()) {
+      throw new MojoExecutionException("Found conflicts in provided deps, see above");
+    }
+  }
+
+  static class ProvidedDepsCollector implements DependencyNodeVisitor {
+    // ArtifactKey -> { scope -> [directArtifact] }
+    private Set<String> deps = new HashSet<>();
+    private int providedDepth = 0;
+
+    @Override
+    public boolean visit(DependencyNode node) {
+      Artifact artifact = node.getArtifact();
+      String scope = artifact.getScope();
+
+      if (providedDepth > 0 || JavaScopes.PROVIDED.equals(scope)) {
+        providedDepth++;
+      }
+
+      if (providedDepth > 0) {
+        deps.add(artifactKey(node.getArtifact()));
+      }
+      return true;
+    }
+
+    @Override
+    public boolean endVisit(DependencyNode node) {
+      if (providedDepth > 0) {
+        providedDepth--;
+      }
+      return true;
+    }
+
+    Set<String> getResults() {
+      return deps;
+    }
+  }
+
+  class ErrorCollector implements DependencyNodeVisitor {
+    // stack[0] = the module being built
+    // stack[1] = direct dep
+    // stack[...] = transitive deps
+    private Stack<DependencyNode> stack = new Stack<>();
+    private Set<String> providedArtifactKeys;
+
+    private List<String> errors = new ArrayList<>();
+
+    ErrorCollector(Set<String> providedArtifactKeys) {
+      this.providedArtifactKeys = providedArtifactKeys;
+    }
+
+    @Override
+    public boolean visit(DependencyNode node) {
+      stack.push(node);
+
+      // Ignore the module root
+      if (stack.size() == 1) {
+        return true;
+      }
+
+      // No need to check top level provided deps
+      if (JavaScopes.PROVIDED.equals(stack.get(1).getArtifact().getScope())) {
+        return true;
+      }
+      // If the transitive dep is already forced to provided
+      if (JavaScopes.PROVIDED.equals(node.getArtifact().getScope())) {
+        return true;
+      }
+
+      String key = artifactKey(node.getArtifact());
+      if (!providedArtifactKeys.contains(key)) {
+        return true;
+      }
+
+      String topLevelKey = artifactKey(stack.get(1).getArtifact());
+      errors.add(
+          String.format("%s imports %s, which is supposed to be provided", topLevelKey, key));
+
+      return true;
+    }
+
+    @Override
+    public boolean endVisit(DependencyNode node) {
+      stack.pop();
+      return true;
+    }
+
+    public List<String> getErrors() {
+      return errors;
+    }
+  }
+
+  static String artifactKey(Artifact a) {
+    return String.join(":", a.getGroupId(), a.getArtifactId(), a.getType(), a.getClassifier());
+  }
+}
