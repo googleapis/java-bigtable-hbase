d1ef3b64e947aaf12efc15a2509c682da2d054b6
Ron Gal <rongal@google.com>
2023-10-02 16:40:53 +0000

    test

--- bigtable-client-core-parent/bigtable-hbase-integration-tests-common/src/test/java/com/google/cloud/bigtable/hbase/TestScan.java
+++ bigtable-client-core-parent/bigtable-hbase-integration-tests-common/src/test/java/com/google/cloud/bigtable/hbase/TestScan.java
@@ -210,7 +210,7 @@ public class TestScan extends AbstractTest {
 
     Scan scan = new Scan();
     scan.withStartRow(rowKeys[0])
-        .withStopRow(rowFollowing(rowKeys[rowsToWrite - 1]))
+        .withStopRow(rowFollowingSameLength(rowKeys[rowsToWrite - 1]))
         .addFamily(COLUMN_FAMILY);
 
     try (ResultScanner resultScanner = table.getScanner(scan)) {
@@ -277,7 +277,7 @@ public class TestScan extends AbstractTest {
 
     Scan scan = new Scan();
     scan.withStartRow(rowKeys[0])
-        .withStopRow(rowFollowing(rowKeys[rowsToWrite - 1]))
+        .withStopRow(rowFollowingSameLength(rowKeys[rowsToWrite - 1]))
         .addFamily(COLUMN_FAMILY);
     int deleteCount = 0;
     try (ResultScanner resultScanner = table.getScanner(scan)) {
