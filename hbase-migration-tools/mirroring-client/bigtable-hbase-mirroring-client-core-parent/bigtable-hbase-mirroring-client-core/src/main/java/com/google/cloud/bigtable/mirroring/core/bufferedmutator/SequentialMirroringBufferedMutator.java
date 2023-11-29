0d9ec408ec1fa037383910c9e29fbe76d44fe236
Igor Bernstein <igorbernstein@google.com>
2023-11-29 10:26:02 -0500

    chore: clean up stale dep refs (#4256)
    * chore: clean up stale dep refs
    
    Change-Id: Ie3bbb33df4b14e7a4326b147c324dc6a0fddd2d5
    
    * format
    
    Change-Id: I15af77d9dd2e093d53ee8173ac53a339fd389c0c
    
    * a couple more missing bits
    
    Change-Id: I896864e031fa3b582094caac34c34f4163bba1d3

--- hbase-migration-tools/mirroring-client/bigtable-hbase-mirroring-client-core-parent/bigtable-hbase-mirroring-client-core/src/main/java/com/google/cloud/bigtable/mirroring/core/bufferedmutator/SequentialMirroringBufferedMutator.java
+++ hbase-migration-tools/mirroring-client/bigtable-hbase-mirroring-client-core-parent/bigtable-hbase-mirroring-client-core/src/main/java/com/google/cloud/bigtable/mirroring/core/bufferedmutator/SequentialMirroringBufferedMutator.java
@@ -49,7 +49,6 @@ import org.apache.hadoop.hbase.client.Connection;
 import org.apache.hadoop.hbase.client.Mutation;
 import org.apache.hadoop.hbase.client.RetriesExhaustedWithDetailsException;
 import org.apache.hadoop.hbase.client.Row;
-import org.checkerframework.checker.nullness.compatqual.NullableDecl;
 
 /**
  * {@link MirroringBufferedMutator} implementation that performs mutations on secondary database
@@ -249,7 +248,7 @@ public class SequentialMirroringBufferedMutator extends MirroringBufferedMutator
         this.mirroringTracer.spanFactory.wrapWithCurrentSpan(
             new FutureCallback<Void>() {
               @Override
-              public void onSuccess(@NullableDecl Void aVoid) {
+              public void onSuccess(Void aVoid) {
                 primaryFlushErrorsReported.set(null);
                 performSecondaryFlush(
                     dataToFlush,
