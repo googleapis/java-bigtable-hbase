2251e877bf945fad75e58df18c178ead488f233e
Igor Berntein <igorbernstein@google.com>
2024-03-19 13:24:05 -0400

    Add an hbase 1x specific MirroringTable class
    Change-Id: Ifb90e7fef01995dac3e4386e5ee623afac259204

--- hbase-migration-tools/mirroring-client/bigtable-hbase-mirroring-client-1.x-parent/bigtable-hbase-mirroring-client-1.x/src/main/java/com/google/cloud/bigtable/mirroring/hbase1_x/MirroringConnection.java
+++ hbase-migration-tools/mirroring-client/bigtable-hbase-mirroring-client-1.x-parent/bigtable-hbase-mirroring-client-1.x/src/main/java/com/google/cloud/bigtable/mirroring/hbase1_x/MirroringConnection.java
@@ -16,10 +16,15 @@
 package com.google.cloud.bigtable.mirroring.hbase1_x;
 
 import com.google.cloud.bigtable.mirroring.core.MirroringConfiguration;
+import com.google.cloud.bigtable.mirroring.core.utils.CallableThrowingIOException;
+import com.google.cloud.bigtable.mirroring.core.utils.mirroringmetrics.MirroringSpanConstants.HBaseOperation;
+import io.opencensus.common.Scope;
 import java.io.IOException;
 import java.util.concurrent.ExecutorService;
 import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.TableName;
 import org.apache.hadoop.hbase.client.ConnectionFactory;
+import org.apache.hadoop.hbase.client.Table;
 import org.apache.hadoop.hbase.security.User;
 
 public class MirroringConnection
@@ -40,5 +45,37 @@ public class MirroringConnection
   public MirroringConnection(MirroringConfiguration mirroringConfiguration, ExecutorService pool)
       throws IOException {
     super(mirroringConfiguration, pool);
+  }
+
+  @Override
+  public Table getTable(final TableName tableName, ExecutorService executorService)
+      throws IOException {
+    try (Scope scope = this.mirroringTracer.spanFactory.operationScope(HBaseOperation.GET_TABLE)) {
+      Log.trace("getTable(%s, executorService)", tableName);
+      Table primaryTable =
+          this.mirroringTracer.spanFactory.wrapPrimaryOperation(
+              new CallableThrowingIOException<Table>() {
+                @Override
+                public Table call() throws IOException {
+                  return getPrimaryConnection().getTable(tableName);
+                }
+              },
+              HBaseOperation.GET_TABLE);
+      Table secondaryTable = this.getSecondaryConnection().getTable(tableName);
+      return new MirroringTable(
+          primaryTable,
+          secondaryTable,
+          executorService,
+          this.mismatchDetector,
+          this.flowController,
+          this.secondaryWriteErrorConsumer,
+          this.readSampler,
+          this.timestamper,
+          this.performWritesConcurrently,
+          this.waitForSecondaryWrites,
+          this.mirroringTracer,
+          this.referenceCounter,
+          this.configuration.mirroringOptions.maxLoggedBinaryValueLength);
+    }
   }
 }
---
+++ hbase-migration-tools/mirroring-client/bigtable-hbase-mirroring-client-1.x-parent/bigtable-hbase-mirroring-client-1.x/src/main/java/com/google/cloud/bigtable/mirroring/hbase1_x/MirroringTable.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.cloud.bigtable.mirroring.hbase1_x;
+
+import com.google.cloud.bigtable.mirroring.core.utils.ReadSampler;
+import com.google.cloud.bigtable.mirroring.core.utils.SecondaryWriteErrorConsumer;
+import com.google.cloud.bigtable.mirroring.core.utils.flowcontrol.FlowController;
+import com.google.cloud.bigtable.mirroring.core.utils.mirroringmetrics.MirroringTracer;
+import com.google.cloud.bigtable.mirroring.core.utils.referencecounting.ReferenceCounter;
+import com.google.cloud.bigtable.mirroring.core.utils.timestamper.Timestamper;
+import com.google.cloud.bigtable.mirroring.core.verification.MismatchDetector;
+import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import org.apache.hadoop.hbase.client.RowMutations;
+import org.apache.hadoop.hbase.client.Table;
+
+public class MirroringTable extends com.google.cloud.bigtable.mirroring.core.MirroringTable {
+  public MirroringTable(
+      Table primaryTable,
+      Table secondaryTable,
+      ExecutorService executorService,
+      MismatchDetector mismatchDetector,
+      FlowController flowController,
+      SecondaryWriteErrorConsumer secondaryWriteErrorConsumer,
+      ReadSampler readSampler,
+      Timestamper timestamper,
+      boolean performWritesConcurrently,
+      boolean waitForSecondaryWrites,
+      MirroringTracer mirroringTracer,
+      ReferenceCounter parentReferenceCounter,
+      int resultScannerBufferedMismatchedResults) {
+    super(
+        primaryTable,
+        secondaryTable,
+        executorService,
+        mismatchDetector,
+        flowController,
+        secondaryWriteErrorConsumer,
+        readSampler,
+        timestamper,
+        performWritesConcurrently,
+        waitForSecondaryWrites,
+        mirroringTracer,
+        parentReferenceCounter,
+        resultScannerBufferedMismatchedResults);
+  }
+}
