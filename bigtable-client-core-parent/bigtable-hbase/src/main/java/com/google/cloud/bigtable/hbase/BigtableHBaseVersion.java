1db664a38a72772e8a6b5a3f3d48fd0bde1e2e89
Mattie Fu <mattiefu@google.com>
2024-04-02 10:04:28 -0400

    chore: update main versions

--- bigtable-client-core-parent/bigtable-hbase/src/main/java/com/google/cloud/bigtable/hbase/BigtableHBaseVersion.java
+++ bigtable-client-core-parent/bigtable-hbase/src/main/java/com/google/cloud/bigtable/hbase/BigtableHBaseVersion.java
@@ -34,7 +34,7 @@ public class BigtableHBaseVersion {
   private static final AtomicBoolean wasInitialized = new AtomicBoolean(false);
 
   // {x-version-update-start:bigtable-client-parent:current}
-  public static final String VERSION = "2.12.1-SNAPSHOT";
+  public static final String VERSION = "2.14.0-SNAPSHOT";
   // {x-version-update-end}
 
   /**
