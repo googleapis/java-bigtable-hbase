8f78fa81fdc6477dcc5ad24cd4a8cb6fdda056c2
Mattie Fu <mattiefu@google.com>
2025-05-01 15:55:55 -0400

    chore: set up 2.14.x branch for LTS (#4482)
    * chore: set up 2.14.x branch for LTS
    
    * run mvn with -X

--- .kokoro/build.sh
+++ .kokoro/build.sh
@@ -51,7 +51,7 @@ javadoc)
     ;;
 integration)
 # clean needed when running more than one IT profile
-    mvn --no-transfer-progress clean verify -B ${INTEGRATION_TEST_ARGS} -Penable-integration-tests -DtrimStackTrace=false -Dclirr.skip=true -DskipUnitTests=true
+    mvn --no-transfer-progress clean verify -B ${INTEGRATION_TEST_ARGS} -Penable-integration-tests -DtrimStackTrace=false -Dclirr.skip=true -DskipUnitTests=true -X
     RETURN_CODE=$?
     ;;
 integration-migration)
