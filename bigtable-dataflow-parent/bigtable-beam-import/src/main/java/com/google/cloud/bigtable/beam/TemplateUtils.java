16255fc52e57365769b45216015450e477256484
Justin <justin.uang@gmail.com>
2026-03-26 09:50:43 -0700

    fix: Add support for overriding the number of retries. Format.

--- bigtable-dataflow-parent/bigtable-beam-import/src/main/java/com/google/cloud/bigtable/beam/TemplateUtils.java
+++ bigtable-dataflow-parent/bigtable-beam-import/src/main/java/com/google/cloud/bigtable/beam/TemplateUtils.java
@@ -106,6 +106,12 @@ public class TemplateUtils {
           BigtableOptionsFactory.BIGTABLE_READ_RPC_ATTEMPT_TIMEOUT_MS_KEY,
           options.getBigtableReadRpcAttemptTimeoutMs());
     }
+    if (options.getBigtableMaxAttempts() != null) {
+      configBuilder.withConfiguration(
+          BigtableOptionsFactory.MAX_SCAN_TIMEOUT_RETRIES,
+          ValueProvider.NestedValueProvider.of(
+              options.getBigtableMaxAttempts(), value -> String.valueOf(value)));
+    }
     return configBuilder.build();
   }
 }
